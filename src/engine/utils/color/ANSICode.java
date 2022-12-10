0b709603de77bbe34a37bf363d4c0a8317b80d76
NextLegacy <mcserezli@gmail.com>
2022-12-10 10:55:28 +0100

    fixed alignments and removed unnecessary parts

--- src/engine/utils/color/ANSICode.java
+++ src/engine/utils/color/ANSICode.java
@@ -19,12 +19,7 @@ public final class ANSICode
     private static final String RESET = "\033[0m";
     
     private static final String FOREGROUND(int r, int g, int b) { return "38;2;" + r + ";" + g + ";" + b; }
-    //private static final String FOREGROUND(int rgb)     { return FOREGROUND((rgb & 0xff0000) >> 16, (rgb & 0x00ff00) >> 8, rgb & 0x0000ff); }
-    //private static final String FOREGROUND(Color color) { return FOREGROUND( color.r              , color.g              , color.b       ); }
-
     private static final String BACKGROUND(int r, int g, int b) { return "48;2;" + r + ";" + g + ";" + b; }
-    //private static final String BACKGROUND(int rgb)     { return BACKGROUND((rgb & 0xff0000) >> 16, (rgb & 0x00ff00) >> 8, rgb & 0x0000ff); }
-    //private static final String BACKGROUND(Color color) { return BACKGROUND( color.r              , color.g              , color.b       ); }
 
     private final String code;
 
@@ -75,11 +70,11 @@ public final class ANSICode
     public String useOn(String string) { return toString() + string + RESET; }
     public String useOn(Object object) { return useOn(object.toString()); }
 
-    public String toString() { return code; }
+    @Override public String toString() { return code; }
 
     public static ANSICodeBuilder build() { return new ANSICodeBuilder(); }
 
-    public static class ANSICodeBuilder
+    public static final class ANSICodeBuilder
     {
         private boolean bold, italic, underline, strike;
         private int fR, fG, fB, bR, bG, bB;
@@ -90,10 +85,10 @@ public final class ANSICode
             fR = fG = fB = bR = bG = bB = -1;
         }
 
-        public ANSICodeBuilder bold()      { bold      = true; return this; }
-        public ANSICodeBuilder italic()    { italic    = true; return this; }
+        public ANSICodeBuilder bold     () { bold      = true; return this; }
+        public ANSICodeBuilder italic   () { italic    = true; return this; }
         public ANSICodeBuilder underline() { underline = true; return this; }
-        public ANSICodeBuilder strike()    { strike    = true; return this; }
+        public ANSICodeBuilder strike   () { strike    = true; return this; }
 
         public ANSICodeBuilder foreground(int r, int g, int b) { fR = r; fG = g; fB = b; return this; }
         public ANSICodeBuilder background(int r, int g, int b) { bR = r; bG = g; bB = b; return this; }
