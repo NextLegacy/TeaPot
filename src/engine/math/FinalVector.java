28e88c503e39793dc6cdcd45fb6e453b667f0c63
NextLegacy <mcserezli@gmail.com>
2022-12-10 10:42:24 +0100

    optimized and extended functionality

--- src/engine/math/FinalVector.java
+++ src/engine/math/FinalVector.java
@@ -4,42 +4,57 @@ import static engine.utils.MathUtils.*;
 
 public final class FinalVector extends Vector4
 {
-    private Vector inverted;
-    private Vector normalized;
-    private double magnitude;
-    private Vector angle;
+    public final double x;
+    public final double y;
+    public final double z;
+    public final double w;
+
+    private final Vector inverted;
+    private final Vector normalized;
+    private final double normalSquared;
+    private final double magnitude;
+    private final Vector angle;
 
     public FinalVector()                                       { this(0, 0, 0, 1);  }
     public FinalVector(double x)                               { this(x, 0, 0, 1);  }
     public FinalVector(double x, double y)                     { this(x, y, 0, 1);  }
     public FinalVector(double x, double y, double z)           { this(x, y, z, 1);  }
-    public FinalVector(double x, double y, double z, double w) { super(x, y, z, w); compute(); }
+    public FinalVector(double x, double y, double z, double w) 
+    { 
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.w = w;
 
-    private FinalVector compute()
-    {
         Vector self = vec(this);
 
-        inverted   = self.inverted  ();             
-        magnitude  = self.magnitude ();
-        normalized = self.normalized();
-        angle      = self.angle     ();
-
-        return this;
+        inverted      = self.inverted        ();             
+        magnitude     = self.magnitude       ();
+        normalized    = self.normalized      ();
+        normalSquared = self.magnitudeSquared();
+        angle         = self.angle           ();
     }
 
-    @Override public Vector inverted  () { return inverted;   }
-    @Override public Vector normalized() { return normalized; }
-    @Override public double magnitude () { return magnitude;  }
-    @Override public Vector angle     () { return angle;      }
+    public final double x() { return x; }
+    public final double y() { return y; }
+    public final double z() { return z; }
+    public final double w() { return w; }
 
-    public static final FinalVector zero = new FinalVector(0, 0, 0, 1);
-    public static final FinalVector one  = new FinalVector(1, 1, 1, 1);
+    @Override public Vector inverted        () { return inverted     ; }
+    @Override public Vector normalized      () { return normalized   ; }
+    @Override public double magnitudeSquared() { return normalSquared; }
+    @Override public double magnitude       () { return magnitude    ; }
+    @Override public Vector angle           () { return angle        ; }
 
-    public static final FinalVector up    = new FinalVector( 0, -1);
-    public static final FinalVector right = new FinalVector( 1,  0);
-    public static final FinalVector down  = new FinalVector( 0,  1);
-    public static final FinalVector left  = new FinalVector(-1,  0);
-    
+    public static final FinalVector zero     = fvec(Vector.zero    );
+    public static final FinalVector one      = fvec(Vector.one     );
+    public static final FinalVector right    = fvec(Vector.right   );
+    public static final FinalVector left     = fvec(Vector.left    );
+    public static final FinalVector down     = fvec(Vector.down    );
+    public static final FinalVector up       = fvec(Vector.up      );
+    public static final FinalVector forward  = fvec(Vector.forward );
+    public static final FinalVector backward = fvec(Vector.backward);
+
     public static Vector fromAngle(double rotation)
     {
         return new Vector(Math.cos(rotation), Math.sin(rotation));
