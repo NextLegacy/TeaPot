e8382374415503f70b7d52c77ec672ce3777f95a
NextLegacy <mcserezli@gmail.com>
2022-12-10 10:51:03 +0100

    started making a MeshRenderer Script

---
+++ src/engine/threed/MeshRenderer.java
@@ -0,0 +1,27 @@
+package engine.threed;
+
+import engine.Script;
+
+public class MeshRenderer extends Script
+{
+    private String layer;
+    private Mesh mesh;
+
+    public MeshRenderer()
+    {
+        setLayer(null); setMesh(null);
+    }
+
+    public Mesh   mesh () { return mesh ; }
+    public String layer() { return layer; }
+
+    public MeshRenderer setLayer(String layer) { this.layer = layer; return this; }
+    public MeshRenderer setMesh (Mesh   mesh ) { this.mesh  = mesh ; return this; }
+
+    @Override
+    protected void render() 
+    {
+        if (image().isNot(layer)) return;
+        if (mesh == null) return;
+    }
+}
