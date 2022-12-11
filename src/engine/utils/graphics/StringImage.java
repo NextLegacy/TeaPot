091570ec15be7a2f96ce3fa0c7ae5f96324d56da
NextLegacy <mcserezli@gmail.com>
2022-12-11 22:17:19 +0100

    works now, but is far from finished

--- src/engine/utils/graphics/StringImage.java
+++ src/engine/utils/graphics/StringImage.java
@@ -2,85 +2,84 @@ package engine.utils.graphics;
 
 import static engine.utils.MathUtils.*;
 
-import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.awt.font.LineMetrics;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 
+import engine.math.FinalVector;
 import engine.math.Vector;
-import engine.math.Vector4;
-import engine.utils.StringUtils;
+import engine.utils.ArrayUtils;
+import engine.utils.ImageUtils;
 
-/*
- * The Purpose of this class is to be able to render Strings.
- * The Idea is as follows:
- *     Get size for the String
- *     Create an BufferedImage with the right Size
- *     Draw the String in the center of the BufferedImage
- *     Draw the BufferedImage on to this Image
- * 
- * This way we have a easy way to implement a custom Font renderer later on.
- * 
- * Also we only have to render the String when it changes.
- * I don't know if Graphics::drawString is that efficient to call every Frame.
- * 
- * Only bad side is maybe, if we change the String realy often, lets say every Frame,
- * then this may be slower and this is probably gonna happen often... hmmm
- * 
- * But even if it is slower, then its okay, not gonna think about it
- * 
- * And its super nice for 3D textures Later on, super easy way of doing 3D Strigs
- *     -> 3D Plane with some String as the Texture and because this is an Image
- *        we can directy use this
- */
+//TODO: add way to set colors of String and optimize
 public class StringImage extends Image 
 {
-    private static final Canvas canvas;
-    //Used for FontMetrics.getLineMetices
-    //Reverse engineered and found out that only RenderingHints are getting used (AntiAliasing and FractionalMetrics)
-    private static final Graphics2D graphics;
-
-    static {
-        canvas = new Canvas();
-        graphics = (Graphics2D) canvas.getGraphics(); 
-        graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING     , RenderingHints.VALUE_ANTIALIAS_ON        );
-    }
-
-    private String string;
-    private Font font;
-    private Vector bounds; //If null, the String is gonna take as much space as possible
-
     public StringImage(String string, Font font)
     {
-        super(new int[0], vec());
-        this.string = string;
-        this.font = font;
+        this(string.split("\n"), font);
     }
 
-    public String string() { return string; }
-    public Font   font  () { return font  ; }
-    public Vector bounds() { return bounds; }
-
-    public void string(String string ) { this.string = string     ; }
-    public void font  (Font font     ) { this.font   = font       ; }
-    public void bounds(Vector4 bounds) { this.bounds = vec(bounds); }
-
-    public void render()
+    public StringImage(String[] lines, Font font)
     {
-        //could use sun.font.FontDesignMetrics.getMetrics(font) here
-        //but using such internal methods is not recommended and may be
-        //in later updates Deprecated because internal methods seem to change uncommunicated
-        //either way, there is not access to it, to get access
-        //we would have to use something like module-info.java
-        //but this is not quite working and I am to lazy to look more into it.
-        
-        FontMetrics metrics = canvas.getFontMetrics(font);
-        LineMetrics lineMetrics = metrics.getLineMetrics(string, graphics);
+        super(calculateSize(lines, font));
+        renderString(this, lines, font);
+    }
 
-        int width  = metrics.stringWidth(string);
-        int height = metrics.getHeight() * ((int)(width / bounds.x()) + StringUtils.count(string, "\n") + 1);
+    private static FinalVector calculateSize(String[] lines, Font font)
+    {
+        final Vector totalSize = vec();
+        
+        for (int i = 0; i < lines.length; i++)
+        {
+            Rectangle2D rect = font.getStringBounds(lines[i], new FontRenderContext(new AffineTransform(), false, false));
+            
+            final double width  = rect.getWidth ();
+            final double height = rect.getHeight();    
+
+            if (width > totalSize.x) totalSize.x = width;
+
+            totalSize.y += height;
+        }
+
+        return fvec(totalSize);
+    } 
+
+    private static void renderString(Image image, String[] lines, Font font)
+    {
+        BufferedImage bufferedImage = ImageUtils.createCompatibleBufferedImage(image.size);
+
+        Graphics2D graphics = bufferedImage.createGraphics();
+        
+        graphics.setFont(font);
+        
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON   );
+        graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+
+        graphics.setColor(Color.white);
+        graphics.fillRect(0, 0, (int) image.size.x, (int) image.size.y);
+
+        graphics.setColor(Color.black);
+
+        FontMetrics fontMetrics = graphics.getFontMetrics(font);
+
+        int y = fontMetrics.getAscent();
+
+        for (int i = 0; i < lines.length; i++)
+        {
+            int x = ((int)image.size.x - fontMetrics.stringWidth(lines[i])) / 2;
+            
+            graphics.drawString(lines[i], x, y);
+            y += fontMetrics.getHeight();
+        }
+
+        ArrayUtils.copy(ImageUtils.getBufferedImageDataArray(bufferedImage), image.colorBuffer, image.pixels);
+
+        graphics.dispose();
     }
 }
