ab6728688b65cdfdafb809f1271d102f064c145c
NextLegacy <mcserezli@gmail.com>
2022-12-11 22:18:09 +0100

    testing StringImages

---
+++ src/testing/ImageTest/StringImagTest.java
@@ -0,0 +1,25 @@
+package testing.ImageTest;
+
+import java.awt.Font;
+
+import engine.utils.color.ANSICode;
+import engine.utils.graphics.StringImage;
+import engine.utils.time.Time;
+
+public class StringImagTest 
+{
+    public static void main(String[] args)
+    {
+        System.out.println(Time.measureTime(StringImagTest::testStringImage) * Time.NANOS_TO_SECONDS);
+    }
+
+    public static void testStringImage()
+    {
+        StringImage stringImage = new StringImage(
+            "Hello World\nTest Test Test\n42\n\nTestTest",
+            new Font("Cascadia Code", Font.BOLD, 500)
+        );
+
+        stringImage.export("rsc/out.png", "png");
+    }
+}
