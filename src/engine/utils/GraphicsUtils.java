219ebfad153648cff6c8a3ba47c2af757dd57b2c
NextLegacy <mcserezli@gmail.com>
2022-12-18 16:12:21 +0100

    optimized version of the bresenham algorithm

--- src/engine/utils/GraphicsUtils.java
+++ src/engine/utils/GraphicsUtils.java
@@ -2,48 +2,75 @@ package engine.utils;
 
 import static engine.utils.MathUtils.*;
 
+import java.util.Arrays;
+
 import engine.math.Vector;
 
 public class GraphicsUtils 
 {
-    public static int[][] bresenham(int x0, int y0, int x1, int y1)  
+    /*
+     * TODO:
+     * Return a two dimensional array that only contains the y values,
+     * the x value will be determined through the index 
+     * 
+     * example:
+     * Given is the following line: 
+ 
+     * [][]
+     *     [][]
+     *         [][] 
+     *
+     * This would be the array for it:
+     * 
+     * [
+     *     [1, 2] // x = 0
+     *     [3, 4] // x = 1
+     *     [5, 6] // x = 2
+     * ]
+     * 
+     * otherwise the array does look like this:
+     * 
+     * [
+     *     [0, 1], // p = (0, 1)
+     *     [0, 2], // p = (0, 2)
+     *     [1, 3], // p = (1, 3)
+     *     [1, 4], // p = (1, 4)
+     *     [2, 5], // p = (2, 5 )
+     *     [2, 6], // p = (2, 6)
+     * ] 
+     * 
+     * Which does take up twice as much memory
+     * 
+     */
+    public static int[] bresenham(int x0, int y0, int x1, int y1)  
     {
-        double dx = Math.abs(x1 - x0);
-        double dy = -Math.abs(y1 - y0);
+        final int dx =  abs(x1 - x0);
+        final int dy =  abs(y1 - y0);
+         
+        final int dxHalf = dx >> 1;
+        final int dyHalf = dy >> 1;
 
-        final double sx = x0 < x1 ? 1 : -1;
-        final double sy = y0 < y1 ? 1 : -1;
-
-        double err = dx + dy;
-        double e2 = 0;
+        final int[] pixels = new int[abs(dy) + 1];
         
-        // this will always be more or equal to the actuall count
-        // + 1 for the special case of a line form (0, 0) - (0, 0) and (0, 0) - (1, 1)
-        int countOfPixels = round(Math.sqrt(dx*dx + dy*dy)) + 1;
+        int error = 0;
+        int pixelsInTotal = 0;
+        int pixelsInRow = 0;
+        int row = 0;
 
-        int count = 0;
-    
-        int[][] pixels = new int[countOfPixels][2];
-
-        while (true) 
+        while (pixelsInTotal <= dx)
         {
-            pixels[count][0] = x0;
-            pixels[count][1] = y0;
-
-            count++;
+            error = (pixelsInTotal * -dy + row * dx - dy + dx);
             
-            if (x0 == x1 && y0 == y1) break;
+            error = (row + 1) * dx + (pixelsInTotal + 1) * -dy;
 
-            e2 = 2 * err;
-            
-            if (e2 > dy) { err += dy; x0 += sx; }
-            if (e2 < dx) { err += dx; y0 += sy; }
+            if (error > dyHalf) { pixelsInRow++; pixelsInTotal++; }
+            if (error < dxHalf) { pixels[row] = pixelsInRow; pixelsInRow = 0; row++; }
         }
+        
+        pixels[row] = pixelsInRow;
 
-        int[][] result = new int[count][2];
+        System.out.println(Arrays.toString(pixels));
 
-        System.arraycopy(pixels, 0, result, 0, count);
-
-        return result;
+        return pixels;
     }    
 }
