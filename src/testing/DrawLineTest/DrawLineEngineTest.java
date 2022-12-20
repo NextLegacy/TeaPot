e4378a6bca06b4d1423462b2c425fcf925ada23e
NextLegacy <mcserezli@gmail.com>
2022-12-20 21:00:21 +0100

    testing line rendering

---
+++ src/testing/DrawLineTest/DrawLineEngineTest.java
@@ -0,0 +1,68 @@
+package testing.DrawLineTest;
+
+import engine.GameObject;
+import engine.Scene;
+import engine.Script;
+import engine.math.FinalVector;
+import engine.math.Vector;
+
+import static engine.utils.MathUtils.*;
+
+import engine.Engine;
+
+public class DrawLineEngineTest 
+{
+    public static void main(String[] args)
+    {
+        Engine engine = new Engine(vec(1080, 720), 60, 60, "main");
+
+        engine.setActiveScene(new LineTestScene());
+
+        engine.activate();
+    }    
+
+    public static class LineTestScene extends Scene
+    {
+        @Override
+        protected void init() 
+        {
+            GameObject gameObject = new GameObject();
+            addGameObject(gameObject);
+            
+            gameObject.addScript(new LineTestScript());
+
+            gameObject.scripts().get(0).activate();
+            gameObject.activate();
+
+        }
+    }
+
+    public static class LineTestScript extends Script
+    {
+        @Override protected void awake() { setActive(true); }
+
+        private FinalVector a;
+        private FinalVector b;
+
+        @Override
+        protected void start() 
+        {
+            a = b = fvec(0, 0);
+        }
+
+        @Override
+        protected void update() 
+        {
+            if (left().isDown())
+                a = mouse().position();
+            if (right().isDown())
+                b = mouse().position();    
+        }
+
+        @Override
+        protected void render() 
+        {
+            image().line(a, b, 4, 1, 0xff00ff00);    
+        }
+    }
+}
