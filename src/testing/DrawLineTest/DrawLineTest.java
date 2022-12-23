db4908501e504da7418714d888336df08f2d74ed
NextLegacy <mcserezli@gmail.com>
2022-12-23 11:46:37 +0100

    testing

--- src/testing/DrawLineTest/DrawLineTest.java
+++ src/testing/DrawLineTest/DrawLineTest.java
@@ -1,8 +1,10 @@
 package testing.DrawLineTest;
 
 import engine.utils.GraphicsUtils;
+import engine.utils.Lambda.Action0;
 import engine.utils.color.Color;
 import engine.utils.graphics.DrawableImage;
+import engine.utils.time.Time;
 
 import static engine.utils.MathUtils.*;
 
@@ -14,17 +16,47 @@ public class DrawLineTest
 {
     public static void main(String[] args)
     {
-        final Vector size = vec(16, 9).times(10);
+        final Vector size = vec(16, 9).times(100);
 
         DrawableImage image = new DrawableImage(size);
 
         image.fillColor(0xffffffff);
 
-        image.line(vec(33, 34), vec(123, 60), 2, 10, 0xff000ff0);
-        //image.line(vec(5,  5), size.minus(vec(5, 5)), 2, (v, t) -> 0xff000000 + Color.lerp(new int[] { 0xf35ba4, 0x11f3ff, 0x0a652f, 0xf0a343 }, t));
-        //image.line(vec(size.x / 2, 0), vec(size.x / 2, size.y), 2, (v, t) -> 0xff000000 + Color.lerp(new int[] { 0xf35ba4, 0x11f3ff, 0x0a652f, 0xf0a343 }, t));
-        //image.line(vec(0, size.y / 2), vec(size.x, size.y / 2), 2, (v, t) -> 0xff000000 + Color.lerp(new int[] { 0xf35ba4, 0x11f3ff, 0x0a652f, 0xf0a343 }, t));
-    
+        final int tests = 100;
+
+        double a = test(tests, () ->  
+        {
+            for (int i = 0; i < 9999; i++)
+                image.bresenham(Vector.random().times(size), Vector.random().times(size), 1, 0xff000000 + ((int) (0xffffff * Math.random())));
+        });
+
+        //double a = test(tests, () ->
+        //{
+        //    for (int i = 0; i < 9999; i++)
+        //        image.line(Vector.random().times(size), Vector.random().times(size), 1, 0xff000000 + ((int) (0xffffff * Math.random())));
+        //});
+
+        a *= Time.NANOS_TO_SECONDS;
+
+        System.out.println(a);
+
+        main(args);
+
         image.export("rsc/out.png", "png");
+    }
+
+    public static double test(int tests, Action0 action)
+    {
+        double result = 0;
+
+        for (int i = 0; i < tests; i++)
+        {
+            result += Time.measureTime(action);
+            //System.out.println("Test " + i + " completed.");
+        }
+
+        result /= tests;
+
+        return result;
     }
 }
