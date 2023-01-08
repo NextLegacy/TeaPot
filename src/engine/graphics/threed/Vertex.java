9f084696017688f0b408afe2f9ae1933c3381a48
NextLegacy <mcserezli@gmail.com>
2023-01-08 23:16:17 +0100

    made Vertex conleable

--- src/engine/graphics/threed/Vertex.java
+++ src/engine/graphics/threed/Vertex.java
@@ -1,32 +1,44 @@
 package engine.graphics.threed;
 
+import engine.math.FinalVector;
 import engine.math.Matrix;
 import engine.math.Vector4;
 
-public final class Vertex 
+public final class Vertex
 {
-    public Vector4 v; //vertex position
-    public Vector4 t; //vertex texture
-    public Vector4 n; //vertex normal
+    public final FinalVector vertex ; //vertex position
+    public final FinalVector texture; //vertex texture
+    public final FinalVector normal ; //vertex normal
 
-    public Vertex(Vector4 v, Vector4 t, Vector4 n)
+    public Vertex(Vector4 vertex, Vector4 texture, Vector4 normal)
     {
-        this.v = v;
-        this.t = t;
-        this.n = n;
+        this.vertex  = vertex .toFinalVector();
+        this.texture = texture.toFinalVector();
+        this.normal  = normal .toFinalVector();
     }
 
     public Vertex transform(Matrix transformation)
     {
-        return new Vertex(
-            transformation.times(v),
-            t,
-            n
+        return new Vertex
+        (
+            transformation.times(vertex),
+            texture,
+            normal
         );
     }
 
     public String toString()
     {
-        return "Vertex(v: " + v.toCompactString() + " | t: " + t.toCompactString() + " | n: " + n.toCompactString() + ")";
+        return "Vertex(v: " + vertex.toCompactString() + " | t: " + texture.toCompactString() + " | n: " + normal.toCompactString() + ")";
+    }
+
+    public Vertex clone()
+    {
+        return new Vertex
+        (
+            vertex .clone(),
+            texture.clone(),
+            normal .clone()
+        );
     }
 }
