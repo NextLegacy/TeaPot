1474b52cb32a0685d1f8c4120b954f4c20982e8c
NextLegacy <mcserezli@gmail.com>
2023-01-20 21:26:52 +0100

    testing and debugging triangle restarizer

--- src/testing/DrawLineTest/DrawLineEngineTest.java
+++ src/testing/DrawLineTest/DrawLineEngineTest.java
@@ -4,7 +4,6 @@ import engine.GameObject;
 import engine.Scene;
 import engine.Script;
 import engine.graphics.Image;
-//import engine.graphics.ImageAlgorithms3D;
 import engine.graphics.threed.Triangle;
 import engine.graphics.threed.Vertex;
 import engine.math.FinalVector;
@@ -20,7 +19,7 @@ public class DrawLineEngineTest
 {
     public static void main(String[] args)
     {
-        Engine engine = new Engine(Screen.get(0), vec(1080, 720), 40, 40, "main");
+        Engine engine = new Engine(Screen.get(0), vec(1080, 720), 60, 60, "main");
 
         engine.setActiveScene(new LineTestScene());
 
@@ -60,25 +59,20 @@ public class DrawLineEngineTest
                 b = mouse().position();  
             if (wheel().isDown())
                 c = mouse().position();
-                b = mouse().position();  
-            if (wheel().isDown())
-                c = mouse().position();
         }
 
         @Override
         protected void render() 
         {
-            //ImageAlgorithms3D.renderTestMeshWithTestImage(image());
-            //image().line(a, b, 30, 40, 0xffff0000);    
-            //ImageAlgorithms3D.triangle(image(), 
-            //    new Triangle
-            //    (
-            //        new Vertex(a, new Vector(0.25, 0.25, 1), a), 
-            //        new Vertex(b, new Vector(0.25, 1   , 1), b), 
-            //        new Vertex(c, new Vector(0.75, 0.25, 1), c)
-            //    ), 
-            //    Image.fromFile("/rsc/images/B.png")
-            //);
+            image().fillTriangle(
+                new Triangle
+                (
+                    new Vertex(a, new Vector(0.25, 0.25, 1), a), 
+                    new Vertex(b, new Vector(0.25, 1   , 1), b), 
+                    new Vertex(c, new Vector(0.75, 0.25, 1), c)
+                ), 
+                Image.fromFile("./rsc/out.png")
+            );
         }
     }
 }
