8827c2ca5cbffbf29d48bc2281ba26a3b23d97e9
NextLegacy <mcserezli@gmail.com>
2023-01-25 23:37:20 +0100

    optimized and cleaned up code, not finished yet

--- src/engine/graphics/ImageAlgorithms3D.java
+++ src/engine/graphics/ImageAlgorithms3D.java
@@ -4,7 +4,6 @@ import engine.math.FinalVector;
 import engine.math.Matrix;
 import engine.math.Vector;
 import engine.math.Vector4;
-import engine.threed.Camera;
 import engine.threed.Mesh;
 import engine.threed.Triangle;
 import engine.threed.Vertex;
@@ -20,14 +19,19 @@ public final class ImageAlgorithms3D
     static void line(final DrawableImage image, 
         final Vertex a, final Vertex b, final Image texture)
     {
-        final double dxd = Math.abs(b.x - a.x);
-        final double dyd = b.y == a.y ? 1 : Math.abs(b.y - a.y);
+        double dxd = abs(b.x - a.x);
+        double dyd = abs(b.y - a.y);
+        
+        final int dx = round(dxd - 0.5);
+        final int dy = round(dyd - 0.5);
 
-        final int dx = (int) Math.abs((int)b.x - (int)a.x);
-        final int dy = (int) Math.abs((int)b.y - (int)a.y);
+        if (dyd == 0) dyd = 1;
+        if (dxd == 0) dxd = 1;
+        
+        double invdxd = 1 / dxd;
 
-        final int sx = ((int) a.x) < ((int) b.x) ? 1 : -1;
-        final int sy = ((int) a.y) < ((int) b.y) ? 1 : -1;
+        final int sx = a.x < b.x ? 1 : -1;
+        final int sy = a.y < b.y ? 1 : -1;
 
         int err = dx - dy;
         int e2 = 0;
@@ -35,23 +39,21 @@ public final class ImageAlgorithms3D
         int x = (int) a.x;
         int y = (int) a.y;
 
-        double xd = a.x;
-        double yd = a.y;
         double z = a.z;
 
         double u = a.texture.x;
         double v = a.texture.y;
         double w = a.texture.z;
 
-        double dl = Math.sqrt(dxd * dxd + dyd * dyd);
+        double z_slope = (b.z - a.z) * invdxd;
+
+        double u_slope = (b.texture.x - a.texture.x) * invdxd;
+        double v_slope = (b.texture.y - a.texture.y) * invdxd;
+        double w_slope = (b.texture.z - a.texture.z) * invdxd;
 
         while (true)
         {
             image.drawPixel(x, y, z, texture.getPixel(u, v, w));
-            
-            double dp = Math.sqrt((xd - x) * (xd - x) + (yd - y) * (yd - y));
-
-            double t = dp / dl;
 
             if (x == (int) b.x && y == (int) b.y) break;
 
@@ -60,11 +62,11 @@ public final class ImageAlgorithms3D
             if (e2 > -dy) { err -= dy; x += sx; }
             if (e2 <  dx) { err += dx; y += sy; }
 
-            z = MathUtils.lerp(a.z, b.z, t);
+            z += z_slope;
 
-            u = MathUtils.lerp(a.texture.x, b.texture.x, t);
-            v = MathUtils.lerp(a.texture.y, b.texture.y, t);
-            w = MathUtils.lerp(a.texture.z, b.texture.z, t);
+            u += u_slope;
+            v += v_slope;
+            w += w_slope;
         }
     }
 
@@ -87,40 +89,29 @@ public final class ImageAlgorithms3D
     static void fillNoneFlatTriangle(final DrawableImage image, 
         final Vertex a, final Vertex b, final Vertex c, final Image texture)
     {
-        final double v_slope = (b.y - a.y) / (c.y - a.y);
+        final double slope = (b.y - a.y) / (c.y - a.y);
 
-        final double v_dcax = (c.x - a.x);
-        final double v_dcay = (c.y - a.y);
+        final double dcax = (c.x - a.x);
+        final double dcay = (c.y - a.y);
 
-        final double x = a.x + (v_slope * v_dcax);
+        final double x = a.x + (slope * dcax);
         final double y = b.y;
 
-        final double dl = Math.sqrt(v_dcax * v_dcax + v_dcay * v_dcay);
-        final double dp = Math.sqrt((x - a.x) * (x - a.x) + (y - a.y) * (y - a.y));
+        final double dl = sqrt(dcax * dcax + dcay * dcay);
+        final double dp = sqrt((x - a.x) * (x - a.x) + (y - a.y) * (y - a.y));
 
         final double t = dp / dl;
 
         Vertex d = new Vertex
         (
-            new FinalVector(
-                a.x + (v_slope * v_dcax),
+            fvec(
+                a.x + (slope * dcax),
                 b.y,
-                MathUtils.lerp(a.z, c.z, t),
-                MathUtils.lerp(a.w, c.w, t)
+                lerp(a.z, c.z, t),
+                lerp(a.w, c.w, t)
             ),
-            new FinalVector
-            (
-                MathUtils.lerp(a.texture.x, c.texture.x, t),
-                MathUtils.lerp(a.texture.y, c.texture.y, t),
-                MathUtils.lerp(a.texture.z, c.texture.z, t)
-            ),
-            new FinalVector
-            (
-                MathUtils.lerp(a.normal.x, c.normal.x, t),
-                MathUtils.lerp(a.normal.y, c.normal.y, t),
-                MathUtils.lerp(a.normal.z, c.normal.z, t),
-                MathUtils.lerp(a.normal.w, c.normal.w, t)
-            )
+            lerp(a.texture, c.texture, t),
+            lerp(a.normal , c.normal , t)
         );
 
         fillBottomFlatTriangle(image, a, b, d, texture);
@@ -130,95 +121,200 @@ public final class ImageAlgorithms3D
     static void fillBottomFlatTriangle(final DrawableImage image, 
         final Vertex a, final Vertex b, final Vertex c, final Image texture)
     {
+        double dab = a.distanceTo(b);
+        double dac = a.distanceTo(c);
+
+        double invdab = 1 / dab;
+        double invdac = 1 / dac;
+
         double invslope1 = (b.x - a.x) / (b.y - a.y);
         double invslope2 = (c.x - a.x) / (c.y - a.y);
 
         double curx1 = a.x;
         double curx2 = a.x;
 
-        final double dl = Math.sqrt((b.x - a.x) * (b.x - a.x) + (b.y - a.y) * (b.y - a.y));
+        double z1 = a.z;
+        double z2 = a.z;
+
+        double w1 = a.w;
+        double w2 = a.w;
+
+        double u1 = a.texture.x;
+        double v1 = a.texture.y;
+        double tw1 = a.texture.z;
+
+        double u2 = a.texture.x;
+        double v2 = a.texture.y;
+        double tw2 = a.texture.z;
+
+        double nx1 = a.normal.x;
+        double ny1 = a.normal.y;
+        double nz1 = a.normal.z;
+
+        double nx2 = a.normal.x;
+        double ny2 = a.normal.y;
+        double nz2 = a.normal.z;
+
+        double z1_slope = (b.z - a.z) * invdab;
+        double z2_slope = (c.z - a.z) * invdac;
+
+        double w1_slope = (b.w - a.w) * invdab;
+        double w2_slope = (c.w - a.w) * invdac;
+
+        double u1_slope = (b.texture.x - a.texture.x) * invdab;
+        double v1_slope = (b.texture.y - a.texture.y) * invdab;
+        double tw1_slope = (b.texture.z - a.texture.z) * invdab;
+
+        double u2_slope = (c.texture.x - a.texture.x) * invdac;
+        double v2_slope = (c.texture.y - a.texture.y) * invdac;
+        double tw2_slope = (c.texture.z - a.texture.z) * invdac;
+
+        double nx1_slope = (b.normal.x - a.normal.x) * invdab;
+        double ny1_slope = (b.normal.y - a.normal.y) * invdab;
+        double nz1_slope = (b.normal.z - a.normal.z) * invdab;
 
         for (int scanlineY = (int) a.y; scanlineY <= b.y; scanlineY++)
         {
-            final double dp = Math.sqrt((curx1 - a.x) * (curx1 - a.x) + (scanlineY - a.y) * (scanlineY - a.y));
-
-            final double t = dp / dl;
-            
-            final Vertex v1 = new Vertex
+            final Vertex vertex1 = new Vertex
             (
-                new FinalVector(
-                    curx1,
-                    scanlineY,
-                    MathUtils.lerp(a.z, b.z, t),
-                    MathUtils.lerp(a.w, b.w, t)
-                ),
-                Vector.lerp(a.texture, b.texture, t),
-                Vector.lerp(a.normal, b.normal, t)
+                fvec(curx1, scanlineY, z1, w1),
+                fvec(u1, v1, tw1),
+                fvec(nx1, ny1, nz1)
             );
 
-            final Vertex v2 = new Vertex
+            final Vertex vertex2 = new Vertex
             (
-                new FinalVector(
-                    curx2,
-                    scanlineY,
-                    MathUtils.lerp(a.z, c.z, t),
-                    MathUtils.lerp(a.w, c.w, t)
-                ),
-                Vector.lerp(a.texture, c.texture, t),
-                Vector.lerp(a.normal, c.normal, t)
+                fvec(curx2, scanlineY, z2, w2),
+                fvec(u2, v2, tw2),
+                fvec(nx2, ny2, nz2)
             );
 
-            line(image, v1, v2, texture);
+            line(image, vertex1, vertex2, texture);
 
             curx1 += invslope1;
             curx2 += invslope2;
+
+            z1 += z1_slope;
+            z2 += z2_slope;
+
+            w1 += w1_slope;
+            w2 += w2_slope;
+
+            u1 += u1_slope;
+            v1 += v1_slope;
+            tw1 += tw1_slope;
+
+            u2 += u2_slope;
+            v2 += v2_slope;
+            tw2 += tw2_slope;
+
+            nx1 += nx1_slope;
+            ny1 += ny1_slope;
+            nz1 += nz1_slope;
         }
     }
 
     static void fillTopFlatTriangle(final DrawableImage image, 
         final Vertex a, final Vertex b, final Vertex c, final Image texture)
     {
+        double dac = a.distanceTo(c);
+        double dbc = b.distanceTo(c);
+
+        double invdac = 1 / dac;
+        double invdbc = 1 / dbc;
+
         double invslope1 = (c.x - a.x) / (c.y - a.y);
         double invslope2 = (c.x - b.x) / (c.y - b.y);
 
         double curx1 = c.x;
         double curx2 = c.x;
 
-        final double dl = Math.sqrt((c.x - a.x) * (c.x - a.x) + (c.y - a.y) * (c.y - a.y));
+        double z1 = c.z;
+        double z2 = c.z;
+
+        double w1 = c.w;
+        double w2 = c.w;
+
+        double u1 = c.texture.x;
+        double v1 = c.texture.y;
+        double tw1 = c.texture.z;
+
+        double u2 = c.texture.x;
+        double v2 = c.texture.y;
+        double tw2 = c.texture.z;
+
+        double nx1 = c.normal.x;
+        double ny1 = c.normal.y;
+        double nz1 = c.normal.z;
+
+        double nx2 = c.normal.x;
+        double ny2 = c.normal.y;
+        double nz2 = c.normal.z;
+
+        double z1_slope = (c.z - a.z) * invdac;
+        double z2_slope = (c.z - b.z) * invdbc;
+
+        double w1_slope = (c.w - a.w) * invdac;
+        double w2_slope = (c.w - b.w) * invdbc;
+
+        double u1_slope = (c.texture.x - a.texture.x) * invdac;
+        double v1_slope = (c.texture.y - a.texture.y) * invdac;
+        double tw1_slope = (c.texture.z - a.texture.z) * invdac;
+
+        double u2_slope = (c.texture.x - b.texture.x) * invdbc;
+        double v2_slope = (c.texture.y - b.texture.y) * invdbc;
+        double tw2_slope = (c.texture.z - b.texture.z) * invdbc;
+
+        double nx1_slope = (c.normal.x - a.normal.x) * invdac;
+        double ny1_slope = (c.normal.y - a.normal.y) * invdac;
+        double nz1_slope = (c.normal.z - a.normal.z) * invdac;
+
+        double nx2_slope = (c.normal.x - b.normal.x) * invdbc;
+        double ny2_slope = (c.normal.y - b.normal.y) * invdbc;
+        double nz2_slope = (c.normal.z - b.normal.z) * invdbc;
 
         for (int scanlineY = (int) c.y; scanlineY > a.y; scanlineY--)
         {
-            final double dp = Math.sqrt((curx1 - a.x) * (curx1 - a.x) + (scanlineY - a.y) * (scanlineY - a.y));
-            final double t = dp / dl;
-
-            final Vertex v1 = new Vertex
+            final Vertex vertex1 = new Vertex
             (
-                new FinalVector(
-                    curx1,
-                    scanlineY,
-                    MathUtils.lerp(a.z, c.z, t),
-                    MathUtils.lerp(a.w, c.w, t)
-                ),
-                Vector.lerp(a.texture, c.texture, t),
-                Vector.lerp(a.normal , c.normal, t)
+                fvec(curx1, scanlineY, z1, w1),
+                fvec(u1, v1, tw1),
+                fvec(nx1, ny1, nz1)
             );
 
-            final Vertex v2 = new Vertex
+            final Vertex vertex2 = new Vertex
             (
-                new FinalVector(
-                    curx2,
-                    scanlineY,
-                    MathUtils.lerp(b.z, c.z, t),
-                    MathUtils.lerp(b.w, c.w, t)
-                ),
-                Vector.lerp(b.texture, c.texture, t),
-                Vector.lerp(b.normal , c.normal, t)
+                fvec(curx2, scanlineY, z2, w2),
+                fvec(u2, v2, tw2),
+                fvec(nx2, ny2, nz2)
             );
 
-            line(image, v1, v2, texture);
-            
+            line(image, vertex1, vertex2, texture);
+
             curx1 -= invslope1;
             curx2 -= invslope2;
+
+            z1 -= z1_slope;
+            z2 -= z2_slope;
+
+            w1 -= w1_slope;
+            w2 -= w2_slope;
+
+            u1 -= u1_slope;
+            v1 -= v1_slope;
+            tw1 -= tw1_slope;
+
+            u2 -= u2_slope;
+            v2 -= v2_slope;
+            tw2 -= tw2_slope;
+
+            nx1 -= nx1_slope;
+            ny1 -= ny1_slope;
+            nz1 -= nz1_slope;
+
+            nx2 -= nx2_slope;
+            ny2 -= ny2_slope;
+            nz2 -= nz2_slope;
         }
     }
 
@@ -243,15 +339,11 @@ public final class ImageAlgorithms3D
     { 
         Triangle[] result = new Triangle[0];
 
-        // Make sure plane normal is indeed normal
 		planeNormal = planeNormal.normalized();
 
-		// Create two temporary storage arrays to classify points either side of plane
-		// If distance sign is positive, point lies on "inside" of plane
 		Vertex[] inside_points  = new Vertex[3]; int nInsidePointCount = 0;
 		Vertex[] outside_points = new Vertex[3]; int nOutsidePointCount = 0;
 
-		// Get signed distance of each point in triangle to plane
 		double d0 = dist(triangle.a, planePosition, planeNormal);
 		double d1 = dist(triangle.b, planePosition, planeNormal);
 		double d2 = dist(triangle.c, planePosition, planeNormal);
@@ -273,33 +365,20 @@ public final class ImageAlgorithms3D
 			outside_points[nOutsidePointCount++] = triangle.c;
 		}
 
-		// Now classify triangle points, and break the input triangle into 
-		// smaller output triangles if required. There are four possible
-		// outcomes...
-
 		if (nInsidePointCount == 0)
-		{
-			// All points lie on the outside of plane, so clip whole triangle
-			// It ceases to exist
-
-			return result; // No returned triangles are valid
+        {
+			return result;
 		}
 
 		if (nInsidePointCount == 3)
 		{
-			// All points lie on the inside of plane, so do nothing
-			// and allow the triangle to simply pass through
-
             result = ArrayUtils.push(result, triangle);
 
-			return result; // Just the one returned original triangle is valid
+			return result;
 		}
 
 		if (nInsidePointCount == 1 && nOutsidePointCount == 2)
 		{
-			// Triangle should be clipped. As two points lie outside
-			// the plane, the triangle simply becomes a smaller triangle
-
             double t = (planePosition.minus(inside_points[0])).dot(planeNormal) / (outside_points[0].minus(inside_points[0])).dot(planeNormal);
 
             Triangle newTriangle = new Triangle(
@@ -318,36 +397,11 @@ public final class ImageAlgorithms3D
 
             result = ArrayUtils.push(result, newTriangle);
 
-			// Copy appearance info to new triangle
-			//out_tri1.col =  in_tri.col;
-			//out_tri1.sym = in_tri.sym;
-//
-			//// The inside point is valid, so keep that...
-			//out_tri1.p[0] = *inside_points[0];
-			//out_tri1.t[0] = *inside_tex[0];
-//
-			//// but the two new points are at the locations where the 
-			//// original sides of the triangle (lines) intersect with the plane
-			//float t;
-			//out_tri1.p[1] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
-			//out_tri1.t[1].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
-			//out_tri1.t[1].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
-			//out_tri1.t[1].w = t * (outside_tex[0]->w - inside_tex[0]->w) + inside_tex[0]->w;
-//
-			//out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[1], t);
-			//out_tri1.t[2].u = t * (outside_tex[1]->u - inside_tex[0]->u) + inside_tex[0]->u;
-			//out_tri1.t[2].v = t * (outside_tex[1]->v - inside_tex[0]->v) + inside_tex[0]->v;
-			//out_tri1.t[2].w = t * (outside_tex[1]->w - inside_tex[0]->w) + inside_tex[0]->w;
-
 			return result; // Return the newly formed single triangle
 		}
 
 		if (nInsidePointCount == 2 && nOutsidePointCount == 1)
 		{
-			// Triangle should be clipped. As two points lie inside the plane,
-			// the clipped triangle becomes a "quad". Fortunately, we can
-			// represent a quad with two new triangles
-
             double t = (planePosition.minus(inside_points[0])).dot(planeNormal) / (outside_points[0].minus(inside_points[0])).dot(planeNormal);
 
             Triangle a = new Triangle(
@@ -442,12 +496,6 @@ public final class ImageAlgorithms3D
                     Triangle projectedTriangle = clippedTriangles[j].transformed(projection);
 
                     final Vector4 offsetView = vec(1, 1);
-
-                    //actully simple but brutalley messy code for now, change later to something way more readable
-                    
-                   //System.out.println(
-                   //    projection
-                   //);
 
                     Vertex a = new Vertex
                     (
