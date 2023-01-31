d0d1ed22106d5b68ce827f23291f4689ab99ae9f
NextLegacy <mcserezli@gmail.com>
2023-01-31 23:14:20 +0100

    minor changes and implemented outputLog() function

--- src/engine/utils/logger/Logger.java
+++ src/engine/utils/logger/Logger.java
@@ -7,37 +7,42 @@ import engine.utils.Lambda.Action1;
 public final class Logger
 {
     private LoggerInformation information;
-    private Action1<LoggedString> out;
+    private Action1<LoggedString> outputStream;
     
     private final ArrayList<LoggedString> log;
 
     private boolean newLine;
 
-    public Logger(Action1<LoggedString> out)
-    {
-        this.information = LoggerInformation.NONE;
-        this.out = out;
-        this.log = new ArrayList<>();
-        this.newLine = true;
-    }
+    public Logger() { this((message) -> {}); }
 
     @SafeVarargs
-    public Logger(Action1<LoggedString>... outs)
+    public Logger(Action1<LoggedString>... outputStreams)
     {
         this((message) -> 
         {
-            for (int i = 0; i < outs.length; i++)
-                outs[i].run(message);
+            for (int i = 0; i < outputStreams.length; i++)
+                outputStreams[i].run(message);
         });
+    }
+
+    public Logger(Action1<LoggedString> outputStream)
+    {
+        this.outputStream = outputStream;
+
+        information = LoggerInformation.NONE;
+
+        log = new ArrayList<>();
+
+        newLine = true;
     }
 
     private Logger logInternal(String message)
     {
-        LoggedString loggedString = new LoggedString(getLevel(), newLine ? information.useOn(message) : message);
+        LoggedString loggedString = newLine ? information.useOn(message, " ") : new LoggedString(getLevel(), message);
 
         newLine = loggedString.plainMessage.lastIndexOf("\n") == loggedString.plainMessage.length() - 1;
 
-        out.run(loggedString);
+        outputStream.run(loggedString);
         log.add(loggedString);
 
         return this;
@@ -71,8 +76,8 @@ public final class Logger
     public Logger logf (String level, String format, Object... args) { final String temp = getLevel(); setLevel(level); return log  (String.format(format, args)).setLevel(temp); }
     public Logger logf (String level, Object format, Object... args) { final String temp = getLevel(); setLevel(level); return logf (format .toString(), args   ).setLevel(temp); }
 
-    public Logger logln(String level, String message) { final String temp = getLevel(); setLevel(level); return log  (message + "\n"             ).setLevel(temp); }
-    public Logger logln(String level, Object message) { final String temp = getLevel(); setLevel(level); return logln(message.toString()         ).setLevel(temp); }
+    public Logger logln(String level, String message) { final String temp = getLevel(); setLevel(level); return logln(message           ).setLevel(temp); }
+    public Logger logln(String level, Object message) { final String temp = getLevel(); setLevel(level); return logln(message.toString()).setLevel(temp); }
 
     public Logger setLevel(String level) { information.setLevel(level); return this; }
     public String getLevel() { return information.getLevel(); }
@@ -81,6 +86,16 @@ public final class Logger
     {
         this.information = information == null ? LoggerInformation.NONE : information; 
         
+        return this;
+    }
+
+    public Logger outputLog(Action1<LoggedString> outputStream) 
+    {
+        this.outputStream = outputStream;
+
+        for (int i = 0; i < log.size(); i++)
+            outputStream.run(log.get(i));
+
         return this;
     }
 }
