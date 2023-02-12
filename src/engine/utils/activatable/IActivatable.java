dcc5d6cca79c84c6a327f1550a3bd3b5599fc604
NextLegacy <mcserezli@gmail.com>
2023-02-12 23:57:00 +0100

    documented IActivateable class

--- src/engine/utils/activatable/IActivatable.java
+++ src/engine/utils/activatable/IActivatable.java
@@ -1,12 +1,41 @@
 package engine.utils.activatable;
 
+/**
+ * The IActivatable interface is used to define an object that can be activated or deactivated. <p>
+ * 
+ * @author NextLegacy
+ * @version 1.0
+ */
 public interface IActivatable 
 {
+    /**
+     * Sets the state of the object to active or inactive. <p>
+     * 
+     * @param state The state to set the object to
+     */
     void setActive(boolean state);
 
+    /**
+     * Activates the object. <p>
+     */
     default void activate  () { setActive(true ); }
+
+    /**
+     * Deactivates the object. <p>
+     */
     default void deactivate() { setActive(false); }    
 
+    /**
+     * Returns true if the object is active. <p>
+     * 
+     * @return true if the object is active
+     */
     boolean isActive();
+
+    /**
+     * Returns true if the object is inactive. <p>
+     * 
+     * @return true if the object is inactive
+     */
     default boolean isNotActive() { return !isActive(); }
 }
