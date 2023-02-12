6a9ac3e6ef6b5d271d939c498c8d256b12471b1e
NextLegacy <mcserezli@gmail.com>
2023-02-12 23:57:58 +0100

    documented Random class

--- src/engine/utils/random/Random.java
+++ src/engine/utils/random/Random.java
@@ -2,6 +2,14 @@ package engine.utils.random;
 
 import engine.utils.time.Time;
 
+/**
+ * A random number generator that uses a stateful random number generator to generate random numbers.
+ * 
+ * Default stateful random number generator is xoshiro256ss.
+ * 
+ * @author NextLegacy
+ * @version 1.0
+ */
 public final class Random
 {
     public final State state;
@@ -10,46 +18,84 @@ public final class Random
     public Random(final long seed) { this.state = new xoshiro256ss(seed); }
     public Random(final State state) { this.state = state; }
 
+    /**
+     * @return a random long
+     */
     public final long next()
     {
         return state.next();
     }
 
+    /**
+     * @return a random double in the range [0, 1)
+     */
     public final double randomDouble()
     {
         return (next() >>> 11) * 0x1.0p-53;
     }
 
+    /**
+     * @param bound the upper bound (exclusive)
+     * @return a random int in the range [0, bound)
+     */
     public final int randomInt(int bound)
     {
         return (int) randomLong(bound);
     }
 
+    /**
+     * @param min the lower bound (inclusive)
+     * @param max the upper bound (inclusive)
+     * @return a random int in the range [min, max]
+     */
     public final int randomInt(int min, int max)
     {
         return randomInt(max - min + 1) + min;
     }
 
+    /**
+     * @param bound the upper bound (exclusive)
+     * @return a random long in the range [0, bound)
+     */
     public final long randomLong(long bound)
     {
         return (next() & Long.MAX_VALUE) % bound;
     }
 
+    /**
+     * @param min the lower bound (inclusive)
+     * @param max the upper bound (inclusive)
+     * @return a random long in the range [min, max]
+     */
     public final long randomLong(int min, int max)
     {
         return randomLong(max - min + 1) + min;
     }
 
+    /**
+     * @param min the lower bound (inclusive)
+     * @param max the upper bound (exclusive)
+     * @return a random double in the range [min, max)
+     */
     public final double randomDouble(double min, double max)
     {
         return randomDouble() * (max - min) + min;
     }
 
+    /**
+     * @param probability the probability of returning true
+     * @return a random boolean
+     */
     public final boolean randomBoolean(double probability)
     {
         return randomDouble() < probability;
     }
 
+    /**
+     * @param length the length of the string
+     * @param charSet the set of characters to choose from
+     * @return a random string of length length using characters from charSet
+     */
     public final String randomString(int length, char[] charSet)
     {
         char[] chars = new char[length];
@@ -60,16 +106,23 @@ public final class Random
         return new String(chars);
     }
 
-    static abstract class State
+
+    /**
+     * A stateful random number generator.
+     * 
+     * @author NextLegacy
+     * @version 1.0
+     */
+    public static abstract class State
     {
         final long[] state;
 
-        State(final long seed)
+        protected State(final long seed)
         {
             this.state = new long[] { seed };
         }
 
-        State(final long seed, final int stateLength)
+        protected State(final long seed, final int stateLength)
         {
             this.state = new long[stateLength];
 
@@ -79,7 +132,7 @@ public final class Random
                 state[i] = splitmix64.next();
         }
 
-        abstract long next();
+        protected abstract long next();
     }
 
     //https://www.wikiwand.com/en/Xorshift#Initialization
@@ -91,7 +144,7 @@ public final class Random
         }
 
         @Override
-        long next() 
+        protected long next() 
         {
             long result = (state[0] += 0x9E3779B97F4A7C15L);
             result = (result ^ (result >> 30)) * 0xBF58476D1CE4E5B9L;
@@ -110,7 +163,7 @@ public final class Random
         }
 
         @Override
-        long next() 
+        protected long next() 
         {
             final long result = rol64(state[1] * 5, 7) * 9;
 
