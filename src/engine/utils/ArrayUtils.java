298afbd089a11a18372d388fb9af6890eb20fd0e
NextLegacy <mcserezli@gmail.com>
2023-02-14 21:42:11 +0100

    implemented filter() method

--- src/engine/utils/ArrayUtils.java
+++ src/engine/utils/ArrayUtils.java
@@ -3,8 +3,7 @@ package engine.utils;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
-import engine.utils.array.ArrayFunctions.*;
-
+import engine.utils.ArrayUtils.ArrayFunctions.*;
 
 public final class ArrayUtils
 {
@@ -889,6 +888,123 @@ public final class ArrayUtils
     @SafeVarargs public static <T> T[] array(final int length, final T... junk) { return newInstance(componentTypeOf(junk), length); }
     @SafeVarargs public static <T> T[] array(final int length, final T[]... junk) { return newInstance(componentTypeOf(junk), length); }
 
+    public static <T> T[] filter(final T[] array, final GenericFilter<T> filter)
+    {
+        final T[] filtered = array(array.length, array);
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static boolean[] filter(final boolean[] array, final BooleanFilter filter)
+    {
+        final boolean[] filtered = new boolean[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static byte[] filter(final byte[] array, final ByteFilter filter)
+    {
+        final byte[] filtered = new byte[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+    
+    public static char[] filter(final char[] array, final CharFilter filter)
+    {
+        final char[] filtered = new char[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static short[] filter(final short[] array, final ShortFilter filter)
+    {
+        final short[] filtered = new short[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static int[] filter(final int[] array, final IntFilter filter)
+    {
+        final int[] filtered = new int[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static long[] filter(final long[] array, final LongFilter filter)
+    {
+        final long[] filtered = new long[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static float[] filter(final float[] array, final FloatFilter filter)
+    {
+        final float[] filtered = new float[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+
+    public static double[] filter(final double[] array, final DoubleFilter filter)
+    {
+        final double[] filtered = new double[array.length];
+
+        int length = 0;
+
+        for (int i = 0; i < array.length; i++)
+            if (filter.filter(array[i]))
+                filtered[length++] = array[i];
+
+        return resize(filtered, length);
+    }
+    
     @SafeVarargs
     public static <T, U> U[] map(final T[] array, final GenericMapper<T, U> mapper, final U... junk)
     {
@@ -1706,5 +1822,113 @@ public final class ArrayUtils
             result[i] = mapper.map(array[i]);
         
         return result;
+    }
+
+    public final class ArrayFunctions
+    {
+        private ArrayFunctions() { }
+
+        @FunctionalInterface public static interface GenericFilter<T> { boolean filter(T value); }
+
+        @FunctionalInterface public static interface BooleanFilter { boolean filter(boolean value); }
+        @FunctionalInterface public static interface ByteFilter    { boolean filter(byte    value); }
+        @FunctionalInterface public static interface CharFilter    { boolean filter(char    value); }
+        @FunctionalInterface public static interface ShortFilter   { boolean filter(short   value); }
+        @FunctionalInterface public static interface IntFilter     { boolean filter(int     value); }
+        @FunctionalInterface public static interface LongFilter    { boolean filter(long    value); }
+        @FunctionalInterface public static interface FloatFilter   { boolean filter(float   value); }
+        @FunctionalInterface public static interface DoubleFilter  { boolean filter(double  value); }
+
+        @FunctionalInterface public static interface GenericMapper<T, U>   { U map(T value); }
+
+        @FunctionalInterface public static interface GenericToBooleanMapper<T> { boolean map(T value); }
+        @FunctionalInterface public static interface GenericToByteMapper   <T> { byte    map(T value); }
+        @FunctionalInterface public static interface GenericToCharMapper   <T> { char    map(T value); }
+        @FunctionalInterface public static interface GenericToShortMapper  <T> { short   map(T value); }
+        @FunctionalInterface public static interface GenericToIntMapper    <T> { int     map(T value); }
+        @FunctionalInterface public static interface GenericToLongMapper   <T> { long    map(T value); }
+        @FunctionalInterface public static interface GenericToFloatMapper  <T> { float   map(T value); }
+        @FunctionalInterface public static interface GenericToDoubleMapper <T> { double  map(T value); }
+
+        @FunctionalInterface public static interface BooleanToGenericMapper<T> { T map(boolean value); }
+        @FunctionalInterface public static interface ByteToGenericMapper   <T> { T map(byte    value); }
+        @FunctionalInterface public static interface CharToGenericMapper   <T> { T map(char    value); }
+        @FunctionalInterface public static interface ShortToGenericMapper  <T> { T map(short   value); }
+        @FunctionalInterface public static interface IntToGenericMapper    <T> { T map(int     value); }
+        @FunctionalInterface public static interface LongToGenericMapper   <T> { T map(long    value); }
+        @FunctionalInterface public static interface FlaotToGenericMapper  <T> { T map(float   value); }
+        @FunctionalInterface public static interface DoubleToGenericMapper <T> { T map(double  value); }
+
+        @FunctionalInterface public static interface BooleanToBooleanMapper { boolean map(boolean value); }
+        @FunctionalInterface public static interface BooleanToByteMapper    { byte    map(boolean value); }
+        @FunctionalInterface public static interface BooleanToCharMapper    { char    map(boolean value); }
+        @FunctionalInterface public static interface BooleanToShortMapper   { short   map(boolean value); }
+        @FunctionalInterface public static interface BooleanToIntMapper     { int     map(boolean value); }
+        @FunctionalInterface public static interface BooleanToLongMapper    { long    map(boolean value); }
+        @FunctionalInterface public static interface BooleanToFloatMapper   { float   map(boolean value); }
+        @FunctionalInterface public static interface BooleanToDoubleMapper  { double  map(boolean value); }
+
+        @FunctionalInterface public static interface ByteToBooleanMapper { boolean map(byte value); }
+        @FunctionalInterface public static interface ByteToByteMapper    { byte    map(byte value); }
+        @FunctionalInterface public static interface ByteToCharMapper    { char    map(byte value); }
+        @FunctionalInterface public static interface ByteToShortMapper   { short   map(byte value); }
+        @FunctionalInterface public static interface ByteToIntMapper     { int     map(byte value); }
+        @FunctionalInterface public static interface ByteToLongMapper    { long    map(byte value); }
+        @FunctionalInterface public static interface ByteToFloatMapper   { float   map(byte value); }
+        @FunctionalInterface public static interface ByteToDoubleMapper  { double  map(byte value); }
+
+        @FunctionalInterface public static interface CharToBooleanMapper { boolean map(char value); }
+        @FunctionalInterface public static interface CharToByteMapper    { byte    map(char value); }
+        @FunctionalInterface public static interface CharToCharMapper    { char    map(char value); }
+        @FunctionalInterface public static interface CharToShortMapper   { short   map(char value); }
+        @FunctionalInterface public static interface CharToIntMapper     { int     map(char value); }
+        @FunctionalInterface public static interface CharToLongMapper    { long    map(char value); }
+        @FunctionalInterface public static interface CharToFloatMapper   { float   map(char value); }
+        @FunctionalInterface public static interface CharToDoubleMapper  { double  map(char value); }
+
+        @FunctionalInterface public static interface ShortToBooleanMapper { boolean map(short value); }
+        @FunctionalInterface public static interface ShortToByteMapper    { byte    map(short value); }
+        @FunctionalInterface public static interface ShortToCharMapper    { char    map(short value); }
+        @FunctionalInterface public static interface ShortToShortMapper   { short   map(short value); }
+        @FunctionalInterface public static interface ShortToIntMapper     { int     map(short value); }
+        @FunctionalInterface public static interface ShortToLongMapper    { long    map(short value); }
+        @FunctionalInterface public static interface ShortToFloatMapper   { float   map(short value); }
+        @FunctionalInterface public static interface ShortToDoubleMapper  { double  map(short value); }
+
+        @FunctionalInterface public static interface IntToBooleanMapper  { boolean map(int value); }
+        @FunctionalInterface public static interface IntToByteMapper     { byte    map(int value); }
+        @FunctionalInterface public static interface IntToCharMapper     { char    map(int value); }
+        @FunctionalInterface public static interface IntToShortMapper    { short   map(int value); }
+        @FunctionalInterface public static interface IntToIntMapper      { int     map(int value); }
+        @FunctionalInterface public static interface IntToLongMapper     { long    map(int value); }
+        @FunctionalInterface public static interface IntToFloatMapper    { float   map(int value); }
+        @FunctionalInterface public static interface IntToDoubleMapper   { double  map(int value); }
+
+        @FunctionalInterface public static interface LongToBooleanMapper { boolean map(long value); }
+        @FunctionalInterface public static interface LongToByteMapper    { byte    map(long value); }
+        @FunctionalInterface public static interface LongToCharMapper    { char   map(long value); }
+        @FunctionalInterface public static interface LongToShortMapper   { short  map(long value); }
+        @FunctionalInterface public static interface LongToIntMapper     { int    map(long value); }
+        @FunctionalInterface public static interface LongToLongMapper    { long   map(long value); }
+        @FunctionalInterface public static interface LongToFloatMapper   { float  map(long value); }
+        @FunctionalInterface public static interface LongToDoubleMapper  { double map(long value); }
+        
+        @FunctionalInterface public static interface FloatToBooleanMapper { boolean map(float value); }
+        @FunctionalInterface public static interface FloatToByteMapper    { byte    map(float value); }
+        @FunctionalInterface public static interface FloatToCharMapper    { char    map(float value); }
+        @FunctionalInterface public static interface FloatToShortMapper   { short   map(float value); }
+        @FunctionalInterface public static interface FloatToIntMapper     { int     map(float value); }
+        @FunctionalInterface public static interface FloatToLongMapper    { long    map(float value); }
+        @FunctionalInterface public static interface FloatToFloatMapper   { float   map(float value); }
+        @FunctionalInterface public static interface FloatToDoubleMapper  { double  map(float value); }
+
+        @FunctionalInterface public static interface DoubleToBooleanMapper { boolean map(double value); }
+        @FunctionalInterface public static interface DoubleToByteMapper    { byte    map(double value); }
+        @FunctionalInterface public static interface DoubleToCharMapper    { char    map(double value); }
+        @FunctionalInterface public static interface DoubleToShortMapper   { short   map(double value); }
+        @FunctionalInterface public static interface DoubleToIntMapper     { int     map(double value); }
+        @FunctionalInterface public static interface DoubleToLongMapper    { long    map(double value); }
+        @FunctionalInterface public static interface DoubleToFloatMapper   { float   map(double value); }
+        @FunctionalInterface public static interface DoubleToDoubleMapper  { double  map(double value); }
     }
 }
