7f9e7679a241dd1e0a78fba36e17774339e7f746
NextLegacy <mcserezli@gmail.com>
2023-02-16 23:54:16 +0100

    added todo for later

--- src/engine/utils/io/Resources.java
+++ src/engine/utils/io/Resources.java
@@ -7,6 +7,24 @@ import engine.utils.Lambda.Func1;
 import engine.graphics.Image;
 import engine.threed.Mesh;
 
+//TODO: refactor this class to be more generic and less specific to meshes and images
+/*
+ * Example Functionality:
+ * Resources r = new Resources();
+ * r.load("mesh", "path/to/mesh.obj");
+ * r.load("image", "path/to/image.png");
+ * r.get("mesh");
+ * r.get("image");
+ * r.remove("mesh");
+ * r.remove("image");
+ * r.clear();
+ * r.load("mesh", "path/to/mesh.obj");
+ * r.set("mesh", meshObject);
+ * r.addLoader("mesh", ThreedUtils::MeshFromObjFile);
+ * r.addLoader("image", Image::fromFile);
+ * r.forEach((name, resource) -> System.out.println(name + " " + resource));
+ */
+
 public final class Resources 
 {
     private Resources() { }
