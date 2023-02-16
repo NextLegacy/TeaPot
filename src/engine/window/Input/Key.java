d13150ca849c69a69e5ccb265282a3be637a5105
NextLegacy <mcserezli@gmail.com>
2023-02-16 23:52:33 +0100

    removed commented out code

--- src/engine/window/Input/Key.java
+++ src/engine/window/Input/Key.java
@@ -5,20 +5,12 @@ public class Key extends Button
     static final Key NULL_KEY = new Key(-1);
 
     final int keyEvent;
-    //boolean isDown;
-    //double timeOnStart;
 
     Key(final int keyEvent)
     {
         super();
         this.keyEvent = keyEvent;
-        //isDown        = false;
-        //timeOnStart   = 0.0d;
     }
 
-    public int keyEvent       () { return keyEvent                                    ; }
-    //public boolean isDown     () { return isDown                                      ; }
-    //public boolean isUp       () { return !isDown                                     ; }
-    //public double  timeOnStart() { return timeOnStart                                 ; }
-    //public double  downTime   () { return isDown ? Time.nanos() - timeOnStart : 0     ; }
+    public int keyEvent() { return keyEvent; }
 }
