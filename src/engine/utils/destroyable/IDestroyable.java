c1d60a9fcedb4ca4581fa21ac9c6b65937939467
NextLegacy <mcserezli@gmail.com>
2023-02-17 23:55:04 +0100

    documented classes

--- src/engine/utils/destroyable/IDestroyable.java
+++ src/engine/utils/destroyable/IDestroyable.java
@@ -1,5 +1,16 @@
 package engine.utils.destroyable;
 
+/**
+ * This interface is used to mark objects that can be destroyed. <p>
+ * 
+ * The {@link #destroy()} method is used to destroy the object. <p>
+ * 
+ * The {@link #isDestroyed()} method is used to check if the object is destroyed. <p>
+ * 
+ * You can use {@link ObjectIsDestroyedException#throwIfIsDestroyed(IDestroyable)} to throw an exception if the object is destroyed. <p>
+ * 
+ * @author NextLegacy
+ */
 public interface IDestroyable 
 {
     void destroy();
--- src/engine/utils/destroyable/ObjectIsDestroyedException.java
+++ src/engine/utils/destroyable/ObjectIsDestroyedException.java
@@ -1,5 +1,8 @@
 package engine.utils.destroyable;
 
+/**
+ * This exception is thrown if an object is destroyed and an action is performed on it. <p>
+ */
 public final class ObjectIsDestroyedException extends RuntimeException
 {
     public ObjectIsDestroyedException(IDestroyable destroyable)
@@ -7,6 +10,14 @@ public final class ObjectIsDestroyedException extends RuntimeException
         super("Can not perform Action, " + destroyable + " is destroyed!");
     }
 
+
+    /**
+     * This method throws an {@link ObjectIsDestroyedException} if the object is destroyed. <p>
+     * This method can be used to validate if an object is destroyed. <p>
+     * 
+     * @param destroyable The object to check if it is destroyed.
+     * @throws ObjectIsDestroyedException If the object is destroyed.
+     */
     public static <T extends IDestroyable> void throwIfIsDestroyed(T destroyable)
     {
         if (destroyable.isDestroyed())
