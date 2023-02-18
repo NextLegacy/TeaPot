ace19f2b1bd6f98af071bac855b545b215667135
NextLegacy <mcserezli@gmail.com>
2023-02-18 09:06:09 +0100

    documented Key class

--- src/engine/window/Input/Key.java
+++ src/engine/window/Input/Key.java
@@ -1,5 +1,13 @@
 package engine.window.Input;
 
+/**
+ * This class represents a key on the keyboard. <p>
+ * 
+ * It extends {@link Button} and has theirfor all the functionality of a button. <p>
+ * 
+ * @author NextLegacy
+ * @version 1.0
+ */
 public class Key extends Button
 {
     static final Key NULL_KEY = new Key(-1);
