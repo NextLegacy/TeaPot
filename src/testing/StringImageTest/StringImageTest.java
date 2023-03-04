67979dc6d181f59845e87ed54dc9b1095b2deaca
NextLegacy <mcserezli@gmail.com>
2023-03-04 22:31:13 +0100

    testing

--- src/testing/StringImageTest/StringImageTest.java
+++ src/testing/StringImageTest/StringImageTest.java
@@ -1,9 +1,15 @@
 package testing.StringImageTest;
 
+import java.awt.Font;
+
+import engine.graphics.string.StringImage;
+
 public class StringImageTest
 {
     public static void main(String[] args) 
     { 
-        System.out.println("Hello World!");
+        StringImage stringImage = new StringImage("Hello World!", new Font("Arial", Font.PLAIN, 12));
+
+        stringImage.export("./test.png", ".png");
     }
 }
