4afc2372170c99b2b41d3002b381c76e15ac1cab
NextLegacy <mcserezli@gmail.com>
2023-03-04 22:28:49 +0100

    moved to engine.graphics.string

--- src/engine/graphics/string/StringImage.java
+++ src/engine/graphics/string/StringImage.java
@@ -1,4 +1,4 @@
-package engine.graphics;
+package engine.graphics.string;
 
 import static engine.utils.MathUtils.*;
 
@@ -12,12 +12,26 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
+import engine.graphics.Image;
 import engine.math.FinalVector;
 import engine.math.Vector;
 import engine.utils.ArrayUtils;
 import engine.utils.ImageUtils;
 
-//TODO: add way to set colors of String and optimize
+/*
+ * TODO: add way to set colors of String and optimize
+ * IDEA: every different part and the parts on a new lime of the text are SimpleStringImages
+ *       this we can also optimize the rendering of the text by only needing to update the parts that changed
+ *       and not the whole text.
+ *       This class represents a text that is split into SimpleStringImages
+ *       and gets rendered as one image.
+ *       Maybe the optimization is not worth the effort and maybe even counterintuitive but it is worth a try.
+ *       Also habing multiple parts could be even useful for non String representing Images, e.g. custom chars in form 
+ *       of images. So maybe what about a general class for combining multiple images into one image? A ImageBuilder? A ImageComposer? A ImageCombiner?
+ *       This could also be used for the StringImage class.
+ *       This would also be beneficial for a UI system.
+ */
+
 public class StringImage extends Image 
 {
     public StringImage(String string, Font font)
@@ -52,7 +66,7 @@ public class StringImage extends Image
 
     private static void renderString(Image image, String[] lines, Font font)
     {
-        BufferedImage bufferedImage = ImageUtils.createCompatibleBufferedImage(image.size);
+        BufferedImage bufferedImage = ImageUtils.createCompatibleBufferedImage(image.size());
 
         Graphics2D graphics = bufferedImage.createGraphics();
         
@@ -62,7 +76,7 @@ public class StringImage extends Image
         graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
 
         graphics.setColor(Color.white);
-        graphics.fillRect(0, 0, (int) image.size.x, (int) image.size.y);
+        graphics.fillRect(0, 0, (int) image.width(), (int) image.height());
 
         graphics.setColor(Color.black);
 
@@ -72,13 +86,13 @@ public class StringImage extends Image
 
         for (int i = 0; i < lines.length; i++)
         {
-            int x = ((int)image.size.x - fontMetrics.stringWidth(lines[i])) / 2;
+            int x = ((int)image.width() - fontMetrics.stringWidth(lines[i])) / 2;
             
             graphics.drawString(lines[i], x, y);
             y += fontMetrics.getHeight();
         }
 
-        ArrayUtils.copy(ImageUtils.getBufferedImageDataArray(bufferedImage), image.colorBuffer, image.pixels);
+        ArrayUtils.copy(ImageUtils.getBufferedImageDataArray(bufferedImage), image.colorBuffer(), image.pixels());
 
         graphics.dispose();
     }
