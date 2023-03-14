d20bff7145e8a552531138107008e441bf9b5345
NextLegacy <mcserezli@gmail.com>
2023-03-14 23:57:20 +0100

    cleaned up

--- src/engine/window/Input/InputAdapter.java
+++ src/engine/window/Input/InputAdapter.java
@@ -21,66 +21,38 @@ import java.awt.event.WindowStateListener;
 public abstract class InputAdapter implements 
     KeyListener, MouseListener, MouseWheelListener, MouseMotionListener, WindowListener, WindowFocusListener, WindowStateListener
 {
-    @Override
-    public void keyPressed(KeyEvent e) { }
+    // KeyListener
+    @Override public void keyPressed (KeyEvent e) { }
+    @Override public void keyReleased(KeyEvent e) { }
+    @Override public void keyTyped   (KeyEvent e) { }
 
-    @Override
-    public void keyReleased(KeyEvent e) { }
+    // MouseListener
+    @Override public void mouseClicked   (MouseEvent e) { }
+    @Override public void mouseEntered   (MouseEvent e) { }
+    @Override public void mouseExited    (MouseEvent e) { }
+    @Override public void mousePressed   (MouseEvent e) { }
+    @Override public void mouseReleased  (MouseEvent e) { }
+    
+    // MouseMotionListener
+    @Override public void mouseMoved     (MouseEvent e) { }
+    @Override public void mouseDragged   (MouseEvent e) { }
 
-    @Override
-    public void keyTyped(KeyEvent e) { }
+    // MouseWheelListener
+    @Override public void mouseWheelMoved(MouseWheelEvent e) { }
 
-    @Override
-    public void mouseClicked(MouseEvent e) { }
+    // WindowListener
+    @Override public void windowClosing     (WindowEvent e) { }
+    @Override public void windowOpened      (WindowEvent e) { }
+    @Override public void windowActivated   (WindowEvent e) { }
+    @Override public void windowClosed      (WindowEvent e) { }
+    @Override public void windowDeactivated (WindowEvent e) { }
+    @Override public void windowDeiconified (WindowEvent e) { }
+    @Override public void windowIconified   (WindowEvent e) { }
 
-    @Override
-    public void mouseDragged(MouseEvent e) { }
+    // WindowFocusListener
+    @Override public void windowGainedFocus (WindowEvent e) { }
+    @Override public void windowLostFocus   (WindowEvent e) { }
 
-    @Override
-    public void mouseEntered(MouseEvent e) { }
-
-    @Override
-    public void mouseExited(MouseEvent e) { }
-
-    @Override
-    public void mouseMoved(MouseEvent e) { }
-
-    @Override
-    public void mousePressed(MouseEvent e) { }
-
-    @Override
-    public void mouseReleased(MouseEvent e) { }
-
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e) { }
-
-    @Override
-    public void windowClosing(WindowEvent e) { }
-
-    @Override
-    public void windowOpened(WindowEvent e) { }
-
-    @Override
-    public void windowActivated(WindowEvent e) { }
-
-    @Override
-    public void windowClosed(WindowEvent e) { }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) { }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) { }
-
-    @Override
-    public void windowIconified(WindowEvent e) { }
-
-    @Override
-    public void windowGainedFocus(WindowEvent e) { }
-
-    @Override
-    public void windowLostFocus(WindowEvent e) { }
-
-    @Override
-    public void windowStateChanged(WindowEvent e) { }
+    // WindowStateListener
+    @Override public void windowStateChanged(WindowEvent e) { }
 }
