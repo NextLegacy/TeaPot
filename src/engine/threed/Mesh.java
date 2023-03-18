689c4f6cf0f0ae448c5ea35e74abe7cf747395a9
NextLegacy <mcserezli@gmail.com>
2023-03-18 20:30:13 +0100

    added ToDo

--- src/engine/threed/Mesh.java
+++ src/engine/threed/Mesh.java
@@ -8,6 +8,7 @@ package engine.threed;
  */
 public final class Mesh 
 {
+    // TODO: optimize this by using a Vertex array and storing the indices for the triangles, way less pointer needed
     public final Triangle[] triangles;
     
     public Mesh(Triangle[] triangles)
