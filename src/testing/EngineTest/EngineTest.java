82f78096ea28f3c258bf6466a980a92040916c44
NextLegacy <mcserezli@gmail.com>
2023-03-21 23:15:58 +0100

    debugging code execution order

--- src/testing/EngineTest/EngineTest.java
+++ src/testing/EngineTest/EngineTest.java
@@ -31,7 +31,7 @@ public class EngineTest extends Scene
 
     public static void main(String[] args) 
     {
-        Engine engine = new Engine(Screen.get(0), vec(1080, 720), 60, 60, "main");
+        Engine engine = new Engine(Screen.get(0), vec(1080, 720), 4, 60, "main");
 
         engine.setActiveScene(new EngineTest());
 
--- src/testing/EngineTest/TestScript.java
+++ src/testing/EngineTest/TestScript.java
@@ -1,27 +1,31 @@
 package testing.EngineTest;
 
 import engine.Script;
+import engine.utils.random.Random;
 
 import static engine.utils.MathUtils.*;
 
 public class TestScript extends Script
 {
+    Random random;
     protected void start() 
     {
+        random = new Random();
         //System.out.println("TestScript.start()");
     }
 
     protected void update() 
     {
         //System.out.println("TestScript.update()");
+
+        System.out.println(engine());
     }
 
     @Override
     protected void render() 
     {
-        image().fillColor(0xffab2341); // pretty nice color, keep in mind.
-        
-        image().drawLine(vec(0, 0), vec(100, 100), 1, 0xff00ffff);
+        for (int i = 0; i < image().pixels(); i++)
+            image().drawPixel(i, 1, 0xff000000 | random.color());
     }
 
     protected void end() 
