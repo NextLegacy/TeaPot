1f39ab1ec2da3b9ce8fd06d5db71d55fd573e6b5
NextLegacy <mcserezli@gmail.com>
2023-03-25 22:01:54 +0100

    tested code execution order

--- src/testing/EngineTest/TestScript.java
+++ src/testing/EngineTest/TestScript.java
@@ -1,6 +1,8 @@
 package testing.EngineTest;
 
 import engine.Script;
+import engine.graphics.DrawableImage;
+import engine.graphics.Image;
 import engine.utils.random.Random;
 
 public class TestScript extends Script
@@ -9,21 +11,23 @@ public class TestScript extends Script
     protected void start() 
     {
         random = new Random();
-        System.out.println("TestScript.start()");
     }
 
     protected void update() 
     {
-        System.out.println("TestScript.update()");
-
-        System.out.println(engine());
+        window().setTitle(engine().toString());
     }
 
     @Override
     protected void render() 
     {
-        for (int i = 0; i < image().pixels(); i++)
-            image().drawPixel(i, 1, 0xff000000 + random.color());
+        image().drawLine(0, 0, 100, 0, 1d, 0xff00ff00);
+
+        //for (int i = 0; i < image().pixels(); i++)
+        //    image.drawPixel(i, 0, random.randomInt(0x00ffffff) | 0xff000000);
+
+        //for (int i = 0; i < image().pixels(); i++)
+        //    image().drawPixel(i, 1, 0xff000000);
     }
 
     protected void end() 
