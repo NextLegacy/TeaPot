bcbe20695d89c63b795e37a619bbe18826e0ddde
NextLegacy <mcserezli@gmail.com>
2023-03-25 22:05:42 +0100

    made render() update-independent

--- src/engine/utils/time/GameLoop.java
+++ src/engine/utils/time/GameLoop.java
@@ -44,6 +44,7 @@ public abstract class GameLoop implements Runnable
     private double tickDeltaTime;
     private double tickDeltaTimeInSeconds;
 
+    // FIXME: actual tps and fps vary from the given tps and fps, investigate on this
     public GameLoop(int TPS, int FPS) { setTPS(TPS); setFPS(FPS); }
 
     public void setTPS(int TPS)
@@ -167,7 +168,7 @@ public abstract class GameLoop implements Runnable
             if (!isActive())
                 break;
 
-            if (deltaF >= 1 && ticks >= 1)
+            if (deltaF >= 1)
             {
                 final long beforeRender = Time.nanos();
 
