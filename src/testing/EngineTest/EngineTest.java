44413fed28f13819163f2086a92e68c00f72c95a
NextLegacy <mcserezli@gmail.com>
2023-03-26 23:58:30 +0200

    testing

--- src/testing/EngineTest/EngineTest.java
+++ src/testing/EngineTest/EngineTest.java
@@ -29,7 +29,7 @@ public class EngineTest extends Scene
 
     public static void main(String[] args) 
     {
-        Engine engine = new Engine(Screen.get(0), vec(1080, 720), 1000, 1000, "main");
+        Engine engine = new Engine(Screen.get(0), vec(1080, 720), 60, 60, "main");
 
         engine.setActiveScene(new EngineTest());
 
