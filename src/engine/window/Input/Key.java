3a5e5d68324873956f6f615cde0628760706ed21
NextLegacy <mcserezli@gmail.com>
2023-04-10 23:41:19 +0200

    added name()

--- src/engine/window/Input/Key.java
+++ src/engine/window/Input/Key.java
@@ -1,5 +1,7 @@
 package engine.window.Input;
 
+import java.awt.event.KeyEvent;
+
 /**
  * This class represents a key on the keyboard. <p>
  * 
@@ -21,4 +23,5 @@ public class Key extends Button
     }
 
     public int keyEvent() { return keyEvent; }
+    public String name () { return KeyEvent.getKeyText(keyEvent); }
 }
