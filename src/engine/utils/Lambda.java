9cc5ccb559a1ddd9806318bbe779906379263bd5
NextLegacy <mcserezli@gmail.com>
2023-04-25 18:35:14 +0200

    changed Lambdas a bit to allow unsafe invokations

--- src/engine/utils/Lambda.java
+++ src/engine/utils/Lambda.java
@@ -1,8 +1,5 @@
 package engine.utils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 /**
  * A bunch of useful methods that can be used as lambda expressions.
  *
@@ -29,54 +26,95 @@ public final class Lambda
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public static abstract interface IAction { }
-
-    @FunctionalInterface public static interface Action extends IAction
-    { public void run(Object... args); }
-
-    @FunctionalInterface public static interface Action0 extends IAction 
-    { public void run(); }
-    
-    @FunctionalInterface public static interface Action1<T1> extends IAction 
-    { public void run(T1 arg1); }
-    
-    @FunctionalInterface public static interface Action2<T1, T2> extends IAction 
-    { public void run(T1 arg1, T2 arg2); }
-    
-    @FunctionalInterface public static interface Action3<T1, T2, T3> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3); }
-    
-    @FunctionalInterface public static interface Action4<T1, T2, T3, T4> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3, T4 arg4); }
-    
-    @FunctionalInterface public static interface Action5<T1, T2, T3, T4, T5> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5); }
-    
-    @FunctionalInterface public static interface Action6<T1, T2, T3, T4, T5, T6> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6); }
-    
-    @FunctionalInterface public static interface Action7<T1, T2, T3, T4, T5, T6, T7> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7); }
-    
-    @FunctionalInterface public static interface Action8<T1, T2, T3, T4, T5, T6, T7, T8> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8); }
-    
-    @FunctionalInterface public static interface Action9<T1, T2, T3, T4, T5, T6, T7, T8, T9> extends IAction 
-    { public void run(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9); }
-
-    public static class ActionList<T extends IAction> extends ArrayList<T>
+    /**
+     * This interaface is the super interface of all lambda expressions.
+     */
+    public static abstract interface ILambda 
     {
-        public void runAll(Action1<T> actionRunner)
-        {
-            for (T action : this) actionRunner.run(action);
+        /**
+         * This method is used to invoke the Lambda expression without knowing the type of it.
+         * This may be useful when you have a list of Lambda expressions and you want to invoke them all.
+         */
+        Object invokeLambda(Object... args);
+    }
+
+    /**
+     * This interface is used to create a lambda expression that does not return anything.
+     */
+    public static abstract interface IAction extends ILambda 
+    {
+        @Override default Object invokeLambda(Object... args) 
+        { 
+            invokeAction(args); 
+            return null; 
+        } 
+
+        /**
+         * This method is used to invoke the action.
+         */
+        public void invokeAction(Object... args); 
+    }
+
+    @FunctionalInterface 
+    public static interface Action extends IAction
+    { 
+        public void invoke(Object... args); 
+    
+        @Override 
+        default void invokeAction(Object... args) 
+        { 
+            invoke(args); 
         }
     }
 
-    public static class ActionHashMap<TKey, T extends IAction> extends HashMap<TKey, T>
-    {
-        public void runAll(Action1<T> actionRunner)
-        {
-            for (T action : this.values()) actionRunner.run(action);
+    @FunctionalInterface 
+    public static interface Action0 extends IAction 
+    { 
+        public void run();
+    
+        @Override 
+        default void invokeAction(Object... args) 
+        { 
+            run(); 
+        }
+    }
+    
+    @FunctionalInterface 
+    public static interface Action1<T1> extends IAction 
+    { 
+        public void run(T1 arg1);
+    
+        @Override 
+        @SuppressWarnings("unchecked")
+        default void invokeAction(Object... args) 
+        { 
+            run((T1) args[0]); 
+        }
+    }
+
+    @FunctionalInterface
+    public static interface Action2<T1, T2> extends IAction 
+    { 
+        public void run(T1 arg1, T2 arg2);
+    
+        @Override 
+        @SuppressWarnings("unchecked")
+        default void invokeAction(Object... args) 
+        { 
+            run((T1) args[0], (T2) args[1]); 
+        }
+    }
+
+    @FunctionalInterface
+    public static interface Action3<T1, T2, T3> extends IAction 
+    { 
+        public void run(T1 arg1, T2 arg2, T3 arg3);
+    
+        @Override 
+        @SuppressWarnings("unchecked")
+        default void invokeAction(Object... args) 
+        { 
+            run((T1) args[0], (T2) args[1], (T3) args[2]); 
         }
     }
 
@@ -84,69 +122,84 @@ public final class Lambda
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    public abstract interface IFunc<TReturn> { }
-
-    @FunctionalInterface public static interface Func extends IFunc<Object>
-    { public Object get(Object... args); }
-
-    @FunctionalInterface public static interface Func0<TReturn> extends IFunc<TReturn>
-    { public TReturn get(); }
-    
-    @FunctionalInterface public static interface Func1<TReturn, T1> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1); }
-    
-    @FunctionalInterface public static interface Func2<TReturn, T1, T2> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1, T2 arg2); }
-    
-    @FunctionalInterface public static interface Func3<TReturn, T1, T2, T3> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3); }
-    
-    @FunctionalInterface public static interface Func4<TReturn, T1, T2, T3, T4> extends IFunc<TReturn>
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3, T4 arg4); }
-    
-    @FunctionalInterface public static interface Func5<TReturn, T1, T2, T3, T4, T5> extends IFunc<TReturn>
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5); }
-    
-    @FunctionalInterface public static interface Func6<TReturn, T1, T2, T3, T4, T5, T6> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6); }
-    
-    @FunctionalInterface public static interface Func7<TReturn, T1, T2, T3, T4, T5, T6, T7> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7); }
-    
-    @FunctionalInterface public static interface Func8<TReturn, T1, T2, T3, T4, T5, T6, T7, T8> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8); }
-    
-    @FunctionalInterface public static interface Func9<TReturn, T1, T2, T3, T4, T5, T6, T7, T8, T9> extends IFunc<TReturn> 
-    { public TReturn get(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9); }
-
-    public static class FuncList<TReturn, T extends IFunc<TReturn>> extends ArrayList<T>
+    /**
+     * This interface is used to create a lambda expression that returns something.
+     * @param <TReturn> The type of the return value.
+     */
+    public abstract interface IFunc<TReturn> extends ILambda 
     {
-        public TReturn[] getAll(Func1<TReturn, T> funcGetter)
+        @Override default Object invokeLambda(Object... args) 
+        { 
+            invokeFunc(args); 
+            return null; 
+        } 
+
+        /**
+         * This method is used to invoke the function and return the result with the specified type.
+         */
+        public TReturn invokeFunc(Object... args); 
+    }
+
+    @FunctionalInterface 
+    public static interface Func extends IFunc<Object>
+    {
+        public Object get(Object... args);
+
+        @Override
+        default Object invokeFunc(Object... args)
         {
-            @SuppressWarnings("unchecked")
-            TReturn[] result = (TReturn[]) new Object[size()];
-
-            for (int i = 0, length = size(); i < length; i++)
-                result[i] = funcGetter.get(this.get(i));
-
-            return result;
+            return get(args);
         }
     }
 
-    public static class FuncHashMap<TReturn, TKey, T extends IFunc<TReturn>> extends HashMap<TKey, T>
+    @FunctionalInterface 
+    public static interface Func0<TReturn> extends IFunc<TReturn>
     {
-        public TReturn[] getAll(Func1<TReturn, TKey> funcGetter)
+        public TReturn get();
+
+        @Override
+        default TReturn invokeFunc(Object... args)
         {
-            @SuppressWarnings("unchecked")
-            TReturn[] result = (TReturn[]) new Object[size()];
+            return get();
+        }
+    }
+    
+    @FunctionalInterface 
+    public static interface Func1<TReturn, T1> extends IFunc<TReturn>
+    {
+        public TReturn get(T1 arg1);
 
-            @SuppressWarnings("unchecked")
-            TKey[] keys = (TKey[]) this.keySet().toArray();
+        @Override
+        @SuppressWarnings("unchecked")
+        default TReturn invokeFunc(Object... args)
+        {
+            return get((T1) args[0]);
+        }
+    }
 
-            for (int i = 0, length = size(); i < length; i++)
-                result[i] = funcGetter.get(keys[i]);
+    @FunctionalInterface 
+    public static interface Func2<TReturn, T1, T2> extends IFunc<TReturn>
+    {
+        public TReturn get(T1 arg1, T2 arg2);
 
-            return result;
+        @Override
+        @SuppressWarnings("unchecked")
+        default TReturn invokeFunc(Object... args)
+        {
+            return get((T1) args[0], (T2) args[1]);
+        }
+    }
+
+    @FunctionalInterface 
+    public static interface Func3<TReturn, T1, T2, T3> extends IFunc<TReturn>
+    {
+        public TReturn get(T1 arg1, T2 arg2, T3 arg3);
+
+        @Override
+        @SuppressWarnings("unchecked")
+        default TReturn invokeFunc(Object... args)
+        {
+            return get((T1) args[0], (T2) args[1], (T3) args[2]);
         }
     }
 }
