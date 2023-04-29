d9034399008fbede8910b3cb441f1c748de912d2
NextLegacy <mcserezli@gmail.com>
2023-04-29 21:22:05 +0200

    testing jni

--- src/testing/jni/JNITest.java
+++ src/testing/jni/JNITest.java
@@ -4,12 +4,16 @@ public class JNITest
 {
     static 
     {
-        System.load("ABSOLUTE_PATH_TO_ENGINE_DLL");
+        //System.load(System.getProperty("user.dir") + "/cpp/dependencies/glfw/lib/glfw3.dll");
+        System.load(System.getProperty("user.dir") + "/cpp/build/Release/vulkan-1.dll");
+        System.load(System.getProperty("user.dir") + "/cpp/build/Release/Engine.dll");
     }
 
     public static void main(String[] args) 
     {
         new JNITest().helloWorld();
+
+        System.out.println("End");
     }
     
     private native void helloWorld();
