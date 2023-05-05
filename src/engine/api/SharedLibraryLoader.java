1d45fc46ae88dc0984c47c661e133f332965b840
NextLegacy <mcserezli@gmail.com>
2023-05-05 23:25:31 +0200

    sharedlibrary loader class

---
+++ src/engine/api/SharedLibraryLoader.java
@@ -0,0 +1,29 @@
+package engine.api;
+
+public final class SharedLibraryLoader 
+{
+    private SharedLibraryLoader() { }
+
+    static
+    {
+        load("bin/Release/Engine.dll");
+    }
+
+    public static void load(String relativePath)
+    {
+        try
+        {
+            System.load(getPath(relativePath));
+        }
+        catch (UnsatisfiedLinkError e)
+        {
+            System.err.println("Failed to load native library: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static String getPath(String relativePath)
+    {
+        return System.getProperty("user.dir") + "/bin/" + relativePath;
+    }
+}
