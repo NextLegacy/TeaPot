1bd40f7261c37a3d02ed3ae1ead7da1cb486dcb5
NextLegacy <mcserezli@gmail.com>
2023-05-06 23:53:04 +0200

    testing

--- cpp/src/testing/app.cpp
+++ cpp/src/testing/app.cpp
@@ -14,8 +14,6 @@ static void glfw_error_callback(int error, const char* description)
 class Application
 {
 public:
-    Application();
-
     const char* glsl_version = "#version 130";
     GLFWwindow* window;
     ImGuiIO& io;
@@ -23,6 +21,11 @@ public:
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    Application() : io(ImGui::GetIO())
+    {
+        
+    }
 
     void Start()
     {
