65f25e71cbb76018037040c3161ff066dac260d4
NextLegacy <mcserezli@gmail.com>
2023-05-07 18:00:12 +0200

    executable cmake

--- cpp/cmake/executable.cmake
+++ cpp/cmake/executable.cmake
@@ -1,12 +1,10 @@
-add_executable(EngineExecutable WIN32 ${FILES})
-target_link_options(EngineExecutable PUBLIC -mwindows)
+add_executable(Engine WIN32 ${FILES})
+target_link_options(Engine PUBLIC -mwindows)
 
-target_include_directories(EngineExecutable PUBLIC ${INCLUDE_DIRS})
-target_compile_definitions(EngineExecutable PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+target_include_directories(Engine PUBLIC ${INCLUDE_DIRS})
+target_link_libraries(Engine PUBLIC ${LIBRARIES})
 
-target_link_libraries(EngineExecutable PUBLIC ${LIBRARIES})
-
-set_target_properties(EngineExecutable PROPERTIES 
+set_target_properties(Engine PROPERTIES 
     #ARCHIVE_OUTPUT_DIRECTORY "../../lib"
     #LIBRARY_OUTPUT_DIRECTORY "../../lib"
     RUNTIME_OUTPUT_DIRECTORY "../../bin"
