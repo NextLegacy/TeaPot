e0acac5580a5eee0d7774f10df84576ca2100591
NextLegacy <mcserezli@gmail.com>
2023-05-14 04:10:50 +0200

    created main.cpp

---
+++ cpp/src/engine/core/main.cpp
@@ -0,0 +1,14 @@
+// Win Main
+#include "main.h"
+#include <Windows.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!a" << std::endl;
+}
+
+int wmain(int argc, wchar_t* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+}
