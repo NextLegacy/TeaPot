86bd03a3e27ea79d7200c962095339875a4186be
NextLegacy <mcserezli@gmail.com>
2023-05-16 10:27:38 +0200

    opengl header

---
+++ cpp/src/engine/core/opengl/opengl.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+namespace Engine
+{
+    namespace OpenGL
+    {
+        class OpenGL
+        {
+        public:
+            GLFWwindow* window;
+        };
+    }
+} 
