e74ef646ca604ad8b2e0cfc26910c2cd41193d38
NextLegacy <mcserezli@gmail.com>
2023-05-16 22:19:36 +0200

    testing

--- cpp/src/engine/core/main.cpp
+++ cpp/src/engine/core/main.cpp
@@ -1,14 +1,8 @@
 // Win Main
 #include "main.h"
-#include <Windows.h>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!a" << std::endl;
-}
-
-int wmain(int argc, wchar_t* argv[])
+int main(int argc, char* argv[])
 {
     std::cout << "Hello World!" << std::endl;
 }
