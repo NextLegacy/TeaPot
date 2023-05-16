117a2afaf493ea413757681d84da53c1e24f3a6d
NextLegacy <mcserezli@gmail.com>
2023-05-16 10:26:20 +0200

    testing

--- cpp/src/engine/core/main.cpp
+++ cpp/src/engine/core/main.cpp
@@ -1,8 +1,30 @@
 // Win Main
 #include "main.h"
 #include <iostream>
+#include <Windows.h>
+
+namespace Engine
+{
+    class Application
+    {
+    public:
+        void Run()
+        {
+            AllocConsole();
+            freopen("CONOUT$", "w", stdout);
+            freopen("CONOUT$", "w", stderr);
+            freopen("CONIN$", "r", stdin);
+
+            std::cout << "Hello World!" << std::endl;
+        }
+    };
+}
 
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello World!" << std::endl;
+    Engine::Application app;
+    
+    app.Run();
+
+    return 0;
 }
