32d9af036a10ae8513ee199f24742f188398d78c
NextLegacy <mcserezli@gmail.com>
2023-05-27 21:33:09 +0200

    fixed wrapper.cpp

--- wrapper/src/wrapper.cpp
+++ wrapper/src/wrapper.cpp
@@ -14,7 +14,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     std::string filePath = "engine_java_demo.jar;./engine_java_core.jar";
     std::string className = " demo.App";
 
-    engine::process::runJar(filePath, className);
+    engine::process::runJar(filePath, className, std::filesystem::current_path().string());
 
     std::cin.get();
 
