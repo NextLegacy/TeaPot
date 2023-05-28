e0fcc4a54c0acc9a6c5724e4c0c1c931208f2c38
NextLegacy <mcserezli@gmail.com>
2023-05-28 21:49:18 +0200

    fixed process

--- core/utils/process.cpp
+++ core/utils/process.cpp
@@ -8,10 +8,12 @@ namespace engine
 
         const std::string java = javaHome + "/bin/java.exe";  
         
-        void runJar(std::string file, std::string className)
+        void runJar(std::string file, std::string className, std::string libName)
         {
-            std::string completeCommand = "\"" + java +  "\"" + " -cp " + file + className;
+            //std::string completeCommand = "\"" + java +  "\"" + " -cp " + file + className;
             
+            std::string completeCommand = "\"" + java +  "\"" + " -cp " + file + " -Djava.library.path=" + libName + className;
+
             system(completeCommand.c_str());
         }
     }
