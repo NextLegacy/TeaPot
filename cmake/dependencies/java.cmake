f51861e86b569814d1790fd72bdf65aeecf4b926
NextLegacy <mcserezli@gmail.com>
2023-06-14 15:09:09 +0200

    added java cmake

---
+++ cmake/dependencies/java.cmake
@@ -0,0 +1,16 @@
+find_package(Java REQUIRED)
+find_package(JNI REQUIRED)
+
+include(UseJava)
+
+set(CMAKE_JAVA_COMPILE_FLAGS "-source" "17" "-target" "17")
+
+set(JAVA_INCLUDE_DIRS   
+    ${JNI_INCLUDE_DIR} 
+    ${_classDir}       # probably not needed
+    ${_stubDir}        # probably not needed
+)
+
+set(JAVA_LIBRARIES
+    ${JNI_LIBRARIES}
+)
