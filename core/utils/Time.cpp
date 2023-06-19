34363537af54ee8fb883df142b256988f4649256
NextLegacy <mcserezli@gmail.com>
2023-06-19 15:25:25 +0200

    added time utitlity

---
+++ core/utils/Time.cpp
@@ -0,0 +1,12 @@
+#include "time.hpp"
+
+namespace DigitalTea
+{
+    namespace Time
+    {
+        uint64_t Now()
+        {
+            return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        }
+    }
+}
