16eb76780b6a9a1b53d78b9eccc9d213fff9a19d
NextLegacy <mcserezli@gmail.com>
2023-07-02 16:21:19 +0200

    restructuring whole project

--- core/graphics/Window.cpp
+++ core/graphics/Window.cpp
@@ -1,13 +1,11 @@
 #include "Window.hpp"
 
-#include <iostream>
-
-
-
-namespace DigitalTea 
+namespace Engine 
 {
-    Window::Window() : m_properties(), m_previous_properties()
+    Window::Window(std::string title, glm::ivec2 size)
+        : m_title(title), m_size(size)
     {
+        Init();
     }
 
     Window::~Window()
@@ -15,7 +13,7 @@ namespace DigitalTea
         glfwTerminate();
     }
 
-    int Window::Init()
+    uint8_t Window::Init()
     {
         glfwSetErrorCallback([](int error, const char* description)
         {
@@ -25,13 +23,15 @@ namespace DigitalTea
         if (!glfwInit())
             return 1;
 
+        // GL 3.0 + GLSL 130
+        m_glsl_version = "#version 130";
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
         //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
         //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 
         // Create window with graphics context
-        m_window = glfwCreateWindow(GetWidth(), GetHeight(), GetTitle().c_str(), nullptr, nullptr);  
+        m_window = glfwCreateWindow(m_size.x, m_size.y + 50, m_title.c_str(), nullptr, nullptr);    
     
         if (m_window == nullptr)
             return 1;
@@ -39,41 +39,85 @@ namespace DigitalTea
         glfwMakeContextCurrent(m_window);
         glfwSwapInterval(1); // Enable vsync
 
-        //glfwSetWindowAttrib(m_window, GLFW_DECORATED, GLFW_FALSE);
-        //glfwSetWindowAttrib(m_window, GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+        if (InitImGui())
+            return 1;
+
+        const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        int x = (mode->width - m_size.x) / 2;
+        int y = (mode->height - m_size.y) / 2;
+        glfwSetWindowPos(m_window, x, y);
+
+        glfwSetWindowAttrib(m_window, GLFW_DECORATED, GLFW_FALSE);
+        glfwSetWindowAttrib(m_window, GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+
+        return 0;
+    }
+
+    uint8_t Window::InitImGui()
+    {
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+
+        m_imgui_context = ImGui::CreateContext();
+
+        m_imgui_io = ImGui::GetIO(); (void)m_imgui_io;
+
+        m_imgui_io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+        m_imgui_io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+        m_imgui_io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+        m_imgui_io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+        //m_imgui_io.ConfigViewportsNoAutoMerge = true;
+        //m_imgui_io.ConfigViewportsNoTaskBarIcon = true;
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+        //ImGui::StyleColorsLight();
+
+        // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+        m_imgui_style = ImGui::GetStyle();
+        if (m_imgui_io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            m_imgui_style.WindowRounding = 0.0f;
+            m_imgui_style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+
+        // Setup Platform/Renderer backends
+        ImGui_ImplGlfw_InitForOpenGL(m_window, true);
+        ImGui_ImplOpenGL3_Init(m_glsl_version);
 
         return 0;
     }
 
     void Window::StartFrame()
     {
-        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
     }
 
     void Window::EndFrame()
     {
-        int display_w, display_h;
-        glfwGetFramebufferSize(m_window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(GetClearColor().x, GetClearColor().y, GetClearColor().z, GetClearColor().w);
-        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        glfwMakeContextCurrent(m_window);
+        if (m_imgui_io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
 
         glfwSwapBuffers(m_window);
     }
 
     void Window::Shutdown()
     {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+
         glfwDestroyWindow(m_window);
         glfwTerminate();
-    }
-
-    void Window::Center()
-    {
-        const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        int x = (mode->width - GetWidth()) / 2;
-        int y = (mode->height - GetHeight()) / 2;
-        glfwSetWindowPos(m_window, x, y);
     }
 }
