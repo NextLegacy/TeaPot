f019266b826c13442e07f2e691aeb09d850c4613
NextLegacy <mcserezli@gmail.com>
2023-07-10 16:40:40 +0200

    moving utilities into bhw

---
+++ BoilingHotWater/utils/Time.cpp
@@ -0,0 +1,12 @@
+#include "time.hpp"
+
+namespace BHW
+{
+    namespace Time
+    {
+        uint64_t Now()
+        {
+            return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        }
+    }
+}
