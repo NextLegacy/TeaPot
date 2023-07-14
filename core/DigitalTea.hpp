818184d3eeb6269755f9505d378a6b7da64fadcd
NextLegacy <mcserezli@gmail.com>
2023-07-14 16:43:18 +0200

    restructuring

---
+++ core/DigitalTea.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+//#include "utils/Time.hpp"
+//#include "utils/Random.hpp"
+//#include "utils/serialization/Serializable.hpp"
+//#include "etc/Component.hpp"    
+//#include "etc/Entity.hpp" 
+
+#include "etc/Engine.hpp"
