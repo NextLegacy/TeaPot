0706bc4bdf30e0a447c6134f47c1e991cd8c9ce5
NextLegacy <mcserezli@gmail.com>
2023-07-22 16:52:27 +0200

    simple engine editor attempt

---
+++ TeaPot/editor/Editor.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <BoilingHotWater>
+
+#include "editor/imgui/ImGuiWindow.hpp"
+
+#include "editor/imgui/windows/Console.hpp"
+
+#include "imgui_internal.h"
+
+namespace TeaPot
+{
+    class Editor : public BHW::Application
+    {
+    private:
+        std::vector<BHW::ImGuiWindow*> m_ImGuiWindows;
+
+        static Editor* s_Instance;
+
+    public:
+        Editor();
+
+        void AddImGuiWindow   (BHW::ImGuiWindow* window);
+        void RemoveImGuiWindow(BHW::ImGuiWindow* window);
+
+    protected:
+        void Start () override;
+        void Update() override;
+        void Render() override;
+        void PerSecond() override;
+    };
+}
