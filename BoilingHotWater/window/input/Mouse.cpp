0f3592263e52e213e306a4f2ee442419fa430d7c
NextLegacy <mcserezli@gmail.com>
2023-08-01 17:09:06 +0200

    added various utilities and core components for the engine

---
+++ BoilingHotWater/window/input/Mouse.cpp
@@ -0,0 +1,12 @@
+#include "window/input/Mouse.hpp"
+
+namespace BHW
+{
+    Mouse::Mouse() :
+        m_position(0.0f, 0.0f),
+        m_left    (),
+        m_right   (),
+        m_middle  ()
+    {
+    }
+}
