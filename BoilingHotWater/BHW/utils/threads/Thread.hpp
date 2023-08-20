66a289cdfc38a602fcb21dbf31e74f0a37d528ec
NextLegacy <mcserezli@gmail.com>
2023-08-20 17:42:06 +0200

    Add Thread class for managing threads

---
+++ BoilingHotWater/BHW/utils/threads/Thread.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <thread>
+
+#include <functional>
+
+#include "BHW/utils/Time.hpp"
+
+namespace BHW
+{
+    class Thread
+    {
+    public:
+        Thread(std::function<void(Thread*)> function);
+        
+        ~Thread();
+
+        void Activate  ();
+
+        void Deactivate();
+
+        void ForceStop ();
+
+        void Join      ();
+
+        inline bool IsRunning () const { return m_isRunning ; }
+        inline bool ShouldStop() const { return m_shouldStop; }
+
+    private:
+        void Run();
+
+    private:
+        std::thread* m_thread;
+
+        bool m_shouldStop;
+        bool m_isRunning;
+
+        std::function<void(Thread*)> m_function;
+    };
+}
