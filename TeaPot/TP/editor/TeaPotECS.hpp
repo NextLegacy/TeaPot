035b0934440b9af9b50aaaedcd036635e8e63e59
NextLegacy <mcserezli@gmail.com>
2023-09-24 09:34:20 +0200

    Add TeaPotECS.hpp file

---
+++ TeaPot/TP/editor/TeaPotECS.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <BHW/ecs/ECS.hpp>
+
+#include <BHW/utils/event/EventSystem.hpp>
+
+#include <BHW/utils/data/Data.hpp>
+#include <BHW/utils/data/FixedData.hpp>
+
+namespace TP
+{
+
+}
