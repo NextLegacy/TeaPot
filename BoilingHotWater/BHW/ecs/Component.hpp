1373a20b81e86245a11352ff423714379cab8c66
NextLegacy <mcserezli@gmail.com>
2023-09-28 22:59:19 +0200

    Update ComponentTypeUUID and ComponentIndex types

--- BoilingHotWater/BHW/ecs/Component.hpp
+++ BoilingHotWater/BHW/ecs/Component.hpp
@@ -1,30 +1,18 @@
 #pragma once
 
+#include <cstdint>
+
 namespace BHW
 {
     // is determined at compile time | determines the index of a componentType
-    typedef uint32_t ComponentTypeUUID;
+    typedef uint64_t ComponentTypeUUID;
 
     // changes undeterministically at runtime | determines the index of a component
-    typedef uint32_t ComponentIndex;
+    typedef uint64_t ComponentIndex;
 
-    // combines of ComponentTypeUUID and ComponentIndex | (ComponentTypeUUID << 32) | (ComponentIndex
+    // combines of ComponentTypeUUID and ComponentIndex | (ComponentTypeUUID << 32) | (ComponentIndex)
     typedef uint64_t ComponentUUID;
 
     template <typename ...TComponents>
     struct ComponentBitMask;
-
-    template <typename ...TComponents>
-    struct ECSComponents 
-    {
-        using TComponentBitMask = ComponentBitMask<TComponents...>;
-
-        constexpr static size_t size = sizeof...(TComponents);
-
-        template <typename TComponent>
-        static constexpr ComponentTypeUUID ComponentTypeUUID()
-        {
-            return (std::is_same_v<TComponent, TComponents> + ...);
-        }
-    };
 }
