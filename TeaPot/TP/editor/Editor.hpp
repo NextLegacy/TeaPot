b7dc173af49850ab79e5c6bd0301299878ace488
NextLegacy <mcserezli@gmail.com>
2023-09-28 09:40:14 +0200

    Commented out TeaPotECS and added TestComponent structs

--- TeaPot/TP/editor/Editor.hpp
+++ TeaPot/TP/editor/Editor.hpp
@@ -3,27 +3,52 @@
 #include <TC/TeaCup.hpp>
 #include <TC/gapi/impl/GLFW_OpenGL_DearImGui.hpp>
 
-#include "TP/editor/TeaPotECS.hpp"
+//#include "TP/editor/TeaPotECS.hpp"
 
 namespace TP
-{
+{/*
+    struct TestComponent_1
+    {
+        uint32_t m_test;
+    };
+
+    struct TestComponent_2
+    {
+        uint32_t m_test;
+    };
+    
+    struct TestComponent_3
+    {
+        uint32_t m_test;
+    };
+
     struct TeaPotData : public 
-        BHW::FixedDataStorages
-        <
-            100,
-            BHW::DataStorage<uint32_t>
+        BHW::DataStorages<
+            BHW::FixedDataStorages                 // Renderable Entitys
+            < 
+                100,
+                BHW::DataStorage<TestComponent_1>,
+                BHW::DataStorage<TestComponent_2>,
+                BHW::DataStorage<TestComponent_3>
+            >,
+            BHW::FixedDataStorages                 // Physics Entitys
+            <
+                100,
+                BHW::DataStorage<TestComponent_1>,
+                BHW::DataStorage<TestComponent_3>
+            >
         >
-    { };
+    { };*/
 
     struct Editor;
 
-    template<typename T>
-    struct TeaPotBaseEventSystem : public TC::TeaCupBaseEvent<T>, public BHW::ECSBaseEventSystem<Editor> 
+    struct TeaPotBaseEvent : public TC::TeaCupBaseEvent
     {
-        inline TeaPotBaseEventSystem(Editor* editor) : BHW::ECSBaseEventSystem<Editor>(editor), TC::TeaCupBaseEvent() { }
+        inline TeaPotBaseEvent(Editor* editor) : TC::TeaCupBaseEvent(), m_editor(editor) { }
+
+    protected:
+        Editor* m_editor;
     };
 
-    struct TeaPotECS : public BHW::ECS<TeaPotBaseEventSystem, TeaPotData> { };
-
-    struct Editor : public TC::TeaCup<TC::GLFW_OpenGL_DearImGui, TeaPotBaseEventSystem>, public TeaPotECS { };
+    struct Editor : public TC::TeaCup<TC::GLFW_OpenGL_DearImGui, TeaPotBaseEvent>/*, public BHW::ECS<TeaPotBaseEvent, TeaPotData>*/ { };
 }
