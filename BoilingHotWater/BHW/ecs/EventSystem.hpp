d88eb908ede254df8e3546a661e05bb5605affba
NextLegacy <mcserezli@gmail.com>
2023-09-29 23:00:07 +0200

    Refactor ECS event system

--- BoilingHotWater/BHW/ecs/EventSystem.hpp
+++ BoilingHotWater/BHW/ecs/EventSystem.hpp
@@ -1,28 +1,16 @@
 #pragma once
 
-//#include "BHW/ecs/ECS.hpp"
-
 #include <memory>
 
+#include "BHW/utils/event/EventSystem.hpp"
+
+#include <forward_list>
+
 namespace BHW
-{/*
-    template <typename TECS>
-    class ECSBaseEventSystem
+{
+    template <typename TECS, typename TEventSystem>
+    struct ECSSystem : public TEventSystem
     {
-    public:
-        inline ECSBaseEventSystem(std::shared_ptr<TECS> ecs) : m_ecs(ecs) { }
-
-    protected:
-        std::shared_ptr<TECS> m_ecs;
+        inline ECSSystem() : TEventSystem() {}
     };
-
-    template <typename TBaseSystem, typename ...TSystems>
-    class ECSSystemArgs { };
-
-    template <typename TECS>
-    class ECSSystem : public EventSystem<TECS::TECSSystemArgs::TBaseEventSystem, TECS::TECSSystemArgs::TSystems>
-    {
-        template <typename ...TArgs>
-        inline ECSSystem(TArgs... args) : EventSystem<TECS::TECSSystemArgs::TBaseSystem, TECS::TECSSystemArgs::TSystems...>(args...) { }
-    };*/
 }
