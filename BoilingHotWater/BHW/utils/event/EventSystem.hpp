c2c0ff62fa2ce018c9c2e8eb7b273548892794a0
NextLegacy <mcserezli@gmail.com>
2023-09-30 11:00:59 +0200

    Refactor EventSystem template in BHW/utils/event

--- BoilingHotWater/BHW/utils/event/EventSystem.hpp
+++ BoilingHotWater/BHW/utils/event/EventSystem.hpp
@@ -7,30 +7,23 @@
 #include <algorithm>
 #include <iostream>
 
-
 namespace BHW
 {
     template <typename TBaseEvent, typename ...TEventSubscribers>
     struct EventSystem
     {
-        static_assert((std::is_base_of_v<TBaseEvent, TEventSubscribers> && ...), "All event systems must derive from the base event system");
+        //static_assert((std::is_base_of_v<TBaseEvent, TEventSubscribers> && ...), "All event systems must derive from the base event system");
 
-        template <typename ...TArgs>
-        inline EventSystem(TArgs... args) : m_systems(TEventSubscribers(args...)...)
-        { 
-            
-        }
+        inline EventSystem() : m_systems(std::make_tuple(TEventSubscribers()...)) { }
 
         template <typename ...TArgs>
         inline void ForEachEventSystem(void (TBaseEvent::*function)(TArgs...), TArgs... args)
         {
-            std::for_each(m_systems.begin(), m_systems.end(), [function, args...](TBaseEvent& system)
-            { 
-                (system.*function)(args...);
-            });
+            ((std::get<TEventSubscribers>(m_systems).*function)(args...), ...);
         }
 
     private:
         std::tuple<TEventSubscribers...> m_systems;
     };
 }
+
