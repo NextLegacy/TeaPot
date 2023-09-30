65c1ffa230bb71d1bddc96b4fda44339bb645212
NextLegacy <mcserezli@gmail.com>
2023-09-30 11:00:38 +0200

    Remove unnecessary code in String.hpp

--- BoilingHotWater/BHW/utils/String.hpp
+++ BoilingHotWater/BHW/utils/String.hpp
@@ -1,18 +1,8 @@
 #pragma once
 
 #include <string>
-#include <array>
-#include <charconv>
 
 namespace BHW
 {
-    inline std::string to_string(const float& f)
-    {
-        std::array<char, 32> buffer;
-        std::to_chars_result result = std::to_chars(buffer.data(), buffer.data() + buffer.size(), f);
-        
-        if (result.ec != std::errc()) return std::string();
-        
-        return std::string(buffer.data(), result.ptr - buffer.data());
-    }
+    inline std::string to_string(const float& f);
 }
