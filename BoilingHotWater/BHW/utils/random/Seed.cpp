35343f457b9cfb17f47bdd388f2a017ed1edb02d
NextLegacy <mcserezli@gmail.com>
2023-10-01 11:01:56 +0200

    Refactor Seed class constructor and conversion operators

--- BoilingHotWater/BHW/utils/random/Seed.cpp
+++ BoilingHotWater/BHW/utils/random/Seed.cpp
@@ -2,20 +2,11 @@
 
 namespace BHW
 {
-    Seed::Seed() : Seed(Time::NanoSeconds()) { }
 
-    Seed::Seed(uint64_t seed) : m_seed(seed) { }
-
+    Seed::Seed(uint64_t    seed) : m_seed(seed)                          { }
+    Seed::Seed(                ) : Seed(NanoSeconds())                   { }
     Seed::Seed(std::string seed) : Seed(std::hash<std::string>{ }(seed)) { }
 
-    template <unsigned int N>
-    void Seed::CreateState(uint64_t (&state)[N]) const
-    {
-        uint64_t seed = m_seed;
-
-        for (unsigned int i = 1; i < N; i++)
-        {
-            state[i] = PRNGAlgorithms::Splitmix64(seed);
-        }
-    }
+    Seed::operator uint64_t   () const { return m_seed                ; }
+    Seed::operator std::string() const { return std::to_string(m_seed); }
 }
