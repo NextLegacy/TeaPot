98db35f1fc3deb3a71b604238f93efd7bb9a6e8f
NextLegacy <mcserezli@gmail.com>
2023-10-07 11:03:49 +0200

    Add App.hpp with necessary includes and dependencies

---
+++ MultiThreading_GLFW_ImGui_Minimum/App.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <windows.h>
+
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <GLFW/glfw3.h>
+
+#include <BHW/EntryPoint>
+
+#include <BHW/utils/console/Console.hpp>
+#include <BHW/utils/threads/Thread.hpp>
+
+#include <BHW/utils/Assert.hpp>
