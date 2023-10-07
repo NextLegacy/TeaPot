a387c4b8965535acbc38d19f99138793ce5ff7e6
NextLegacy <mcserezli@gmail.com>
2023-10-07 11:04:05 +0200

    Add App.hpp with necessary includes and dependencies

---
+++ MultiThreading_GLFW_Minimum/App.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windows.h>
+
+#include <GLFW/glfw3.h>
+
+#include <BHW/EntryPoint>
+
+#include <BHW/utils/console/Console.hpp>
+#include <BHW/utils/threads/Thread.hpp>
+
+#include <BHW/utils/Assert.hpp>
