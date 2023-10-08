edd11153c98e75ab65335e9f11e18bf659fedb97
NextLegacy <mcserezli@gmail.com>
2023-10-08 11:05:12 +0200

    Refactor GAPI.hpp interface

--- TeaCup/TC/gapi/GAPI.hpp
+++ TeaCup/TC/gapi/GAPI.hpp
@@ -14,29 +14,35 @@ namespace TC
     public:
         GAPI();
 
-        virtual int  InitializeWindow  () = 0;
-        virtual void InitializeInput   () = 0;
-        
+    public:
+        virtual int  InitializeWindow() = 0;
+
+        virtual void InitializeInput () = 0;
+
+        virtual void InitializeFrameThread() = 0;
+
         virtual void ProcessEvents() = 0;
+
         virtual void RenderFrame  () = 0;
 
         virtual void TerminateWindow() = 0;
 
         virtual bool IsWindowOpen() = 0;
 
-        virtual void SetTitle(const std::string& title) = 0;
-        virtual void SetSize (const glm::ivec2&  size)  = 0;
-    
-        inline TC::Input* GetInput() { return m_input; }
+    public:
+        virtual void SetTitle   (const std::string& title   ) = 0;
+        virtual void SetSize    (const glm::ivec2&  size    ) = 0;
+        //virtual void SetPosition(const glm::ivec2&  position) = 0;
+
+    public:
+        inline Input&     GetInput() { return m_input; }
         inline glm::ivec2 GetSize () { return m_size ; }
 
     protected:
         virtual void Frame() = 0;
 
     protected:
-        glm::ivec2 m_size;
-
-    private:
-        TC::Input* m_input;
+        glm::ivec2 m_size ;
+        Input      m_input;
     };
 }
