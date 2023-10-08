11876da3b032cfe7d09a61316e0d4e2341d1588f
NextLegacy <mcserezli@gmail.com>
2023-10-08 11:04:57 +0200

    Refactor TeaCupRuntime.cpp for improved performance and readability

--- TeaCup/TC/TeaCupRuntime.cpp
+++ TeaCup/TC/TeaCupRuntime.cpp
@@ -3,47 +3,91 @@
 namespace TC
 {
     ApplicationRuntime::ApplicationRuntime() : 
-        m_deltaTickTime          (0),
-        m_deltaFixedTickTime     (0), 
-        m_deltaFrameTime         (0),
-        m_measuredFramesPerSecond(0), 
-        m_measuredTicksPerSecond (0), 
-        m_tps (60), 
-        m_ftps(100),
-        m_fps (60),
-        m_tickThread     (std::bind(&ApplicationRuntime::TickThread     , this)),
-        m_fixedTickThread(std::bind(&ApplicationRuntime::FixedTickThread, this))
+        m_deltaUpdateTime     (0),
+        m_deltaFixedUpdateTime(0),
+        m_deltaFrameTime      (0),
+
+        m_fps (60 ),
+        m_fups(100),
+
+        m_measuredFramesPerSecond      (0),
+        m_measuredUpdatesPerSecond     (0),
+        m_measuredFixedUpdatesPerSecond(0),
+
+        m_fixedUpdateThread(std::bind(&ApplicationRuntime::FixedUpdateThread, this)),
+        m_renderThread     (std::bind(&ApplicationRuntime::RenderThread    , this)),
+
+        m_isRunning(false)
     {
 
     }
 
-    void ApplicationRuntime::StartRuntime()
+    void ApplicationRuntime::StartRuntime() 
     {
-        m_tickThread.Activate();
-        m_fixedTickThread.Activate();
+        m_isRunning = true;
 
-        FrameThread();
-
-        m_tickThread.Deactivate();
-        m_fixedTickThread.Deactivate();
+        MainThread();
     }
 
-    void ApplicationRuntime::FixedTickThread()
+    void ApplicationRuntime::EndRuntime  () { }
+
+    void ApplicationRuntime::MainThread()
+    {
+        // fixing tps
+        uint64_t deltaTime = 0;
+        uint64_t deltaT    = 0;
+        uint64_t last      = BHW::NanoSeconds();
+
+        // measuring tick time
+        uint64_t lastTickTime = BHW::NanoSeconds();
+
+        RuntimeMainThreadStart();
+    
+        m_renderThread.Activate();
+
+        while (IsRunning())
+        {
+            //if (deltaT >= 1e9)
+            {
+                m_deltaUpdateTime = (float) (BHW::NanoSeconds() - lastTickTime) / 1.0e9f;
+                lastTickTime      = BHW::NanoSeconds();
+
+                m_measuredUpdatesPerSecond = 1 / m_deltaUpdateTime;
+
+                RuntimeUpdate();
+
+            //    deltaT -= 1e9;
+            }
+
+            deltaTime  = BHW::NanoSeconds() - last;
+            last      += deltaTime;
+            deltaT    += deltaTime * 60;
+        }
+
+        RuntimeMainThreadEnd();
+
+        m_renderThread.Deactivate();
+    }
+
+
+    void ApplicationRuntime::FixedUpdateThread()
     {
         // measuring fixed ticks per second
-        uint64_t measurementStartTime  = BHW::Time::NanoSeconds();
+        uint64_t measurementStartTime  = BHW::NanoSeconds();
         uint64_t measurementSamples    = 1000;
-        uint64_t measurementFixedTicks = 0;
+        uint64_t measurementFixedUpdates = 0;
 
         // fixing ftps
         uint64_t deltaTime   = 0;
         float    accumulator = 0;
-        float    slice       = 1.0e9f / (float) m_ftps;
-        uint64_t last        = BHW::Time::NanoSeconds();
+        float    slice       = 1.0e9f / (float) m_fups;
+        uint64_t last        = BHW::NanoSeconds();
+
+        RuntimeFixedUpdateThreadStart();
 
         while (IsRunning())
         {
-            deltaTime    = BHW::Time::NanoSeconds() - last;
+            deltaTime    = BHW::NanoSeconds() - last;
             last        += deltaTime;
             accumulator += deltaTime;
 
@@ -51,96 +95,62 @@ namespace TC
 
             while (accumulator > slice)
             {
-                m_deltaFixedTickTime = slice / 1e9f;
+                m_deltaFixedUpdateTime = slice / 1e9f;
 
-                FixedTick(); 
+                RuntimeFixedUpdate(); 
                 
-                measurementFixedTicks++;
+                measurementFixedUpdates++;
 
                 accumulator -= slice;
             }
 
-            if (atLeastOne)
-            {
-                std::lock_guard<std::mutex> lock(m_tickMutex);
-                m_fixedTicksDone = true;
-                m_tickCondition.notify_one();
-            }
+            m_measuredFixedUpdatesPerSecond = ((float) measurementFixedUpdates / ((float)(BHW::NanoSeconds() - measurementStartTime))) * 1e9f;
 
-            m_measuredFixedTicksPerSecond = ((float) measurementFixedTicks / ((float)(BHW::Time::NanoSeconds() - measurementStartTime))) * 1e9f;
-
-            if (measurementFixedTicks >= measurementSamples)
+            if (measurementFixedUpdates >= measurementSamples)
             {
-                measurementStartTime  = BHW::Time::NanoSeconds();
-                measurementFixedTicks = 0;
+                measurementStartTime  = BHW::NanoSeconds();
+                measurementFixedUpdates = 0;
             }
         }
+
+        RuntimeFixedUpdateThreadEnd();
     }
 
-    void ApplicationRuntime::TickThread()
-    {
-        // fixing tps
-        uint64_t deltaTime = 0;
-        uint64_t deltaT    = 0;
-        uint64_t last      = BHW::Time::NanoSeconds();
-
-        // measuring tick time
-        uint64_t lastTickTime = BHW::Time::NanoSeconds();
-
-        while (IsRunning())
-        {
-            if (deltaT >= 1e9)
-            {
-                m_deltaTickTime = (float) (BHW::Time::NanoSeconds() - lastTickTime) / 1.0e9f;
-                lastTickTime    = BHW::Time::NanoSeconds();
-
-                m_measuredTicksPerSecond = 1 / m_deltaTickTime;
-
-                Tick();
-
-                deltaT -= 1e9;
-            }
-
-            //{
-            //    m_fixedTicksDone = false;
-            //    std::unique_lock<std::mutex> lock(m_tickMutex);
-            //    m_tickCondition.wait(lock, [this] { return m_fixedTicksDone; });
-            //    m_fixedTicksDone = false;
-            //}
-
-            deltaTime  = BHW::Time::NanoSeconds() - last;
-            last      += deltaTime;
-            deltaT    += deltaTime * m_tps;
-        }
-    }
-
-    void ApplicationRuntime::FrameThread()
+    void ApplicationRuntime::RenderThread()
     {
         // fixing fps
         uint64_t deltaTime = 0;
         uint64_t deltaF    = 0;
-        uint64_t last      = BHW::Time::NanoSeconds();
+        uint64_t last      = BHW::NanoSeconds();
 
         // measuring frame time
-        uint64_t lastFrameTime = BHW::Time::NanoSeconds();
+        uint64_t lastFrameTime = BHW::NanoSeconds();
+
+        RuntimeRenderThreadStart();
+
+        m_fixedUpdateThread.Activate();
 
         while (IsRunning())
         {
             if (deltaF >= 1e9)
             {
-                m_deltaFrameTime = (float) (BHW::Time::NanoSeconds() - lastFrameTime) / 1.0e9f;
-                lastFrameTime = BHW::Time::NanoSeconds();
+                m_deltaFrameTime = (float) (BHW::NanoSeconds() - lastFrameTime) / 1.0e9f;
+                lastFrameTime = BHW::NanoSeconds();
 
                 m_measuredFramesPerSecond = 1 / m_deltaFrameTime;
 
-                Frame(); 
+                RuntimeRender(); 
 
                 deltaF -= 1e9;
             }
 
-            deltaTime = BHW::Time::NanoSeconds() - last;
+            deltaTime = BHW::NanoSeconds() - last;
             last     += deltaTime;
             deltaF   += deltaTime * m_fps;
         }
+
+        RuntimeRenderThreadEnd();
+
+        m_fixedUpdateThread.Deactivate();
     }
 }
