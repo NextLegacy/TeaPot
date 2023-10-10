55abe623f4959591da2c1c1ec3110b2074a8ecc6
NextLegacy <mcserezli@gmail.com>
2023-10-10 12:10:40 +0200

    Add InputListener class and update MouseWheel accessor

--- TeaCup/TC/input/Input.hpp
+++ TeaCup/TC/input/Input.hpp
@@ -3,12 +3,21 @@
 #include <vector>
 
 #include <BHW/utils/console/Console.hpp>
+#include <BHW/utils/event/DynamicEventSystem.hpp>
 
 #include "TC/input/KeyBoard.hpp"
 #include "TC/input/Mouse.hpp"
 
 namespace TC
 {
+    class InputListener
+    {
+        virtual void Keyboard   (int    key    , int    scancode, int action, int mods) { }
+        virtual void Mouse      (int    button ,                  int action, int mods) { }
+        virtual void MouseScroll(double xoffset, double yoffset                       ) { }
+        virtual void MouseMove  (double xpos   , double ypos                          ) { }
+    };
+
     class Input
     {
     public:
@@ -19,12 +28,14 @@ namespace TC
         inline TC::Key&         Key          (KeyCode         code) { return m_keyBoard(code); }
         inline TC::MouseButton& MouseButton  (MouseButtonCode code) { return m_mouse   (code); }
 
-        inline TC::Mouse&      Mouse          () { return m_mouse             ; }
-        inline glm::vec2       MousePosition  () { return m_mouse.Position  (); }
-        inline TC::MouseWheel  MouseWheel     () { return m_mouse.Wheel     (); }
-        inline glm::vec2       MouseWheelDelta() { return MouseWheel().Delta(); }
-        inline TC::MouseButton MouseLeft      () { return m_mouse.Left      (); }
-        inline TC::MouseButton MouseRight     () { return m_mouse.Right     (); }
+        inline TC::Mouse&       Mouse          () { return m_mouse             ; }
+        inline glm::vec2        MousePosition  () { return m_mouse.Position  (); }
+
+        inline TC::MouseWheel&  MouseWheel     () { return m_mouse.Wheel     (); }
+        inline glm::vec2        MouseWheelDelta() { return MouseWheel().Delta(); }
+
+        inline TC::MouseButton& MouseLeft      () { return m_mouse.Left      (); }
+        inline TC::MouseButton& MouseRight     () { return m_mouse.Right     (); }
 
     public:
         void KeyboardKeyCallback(int    key    , int    scancode, int action, int mods);
