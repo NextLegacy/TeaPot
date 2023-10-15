ff2ce48252e51ba31fada056e98a3287f164168a
NextLegacy <mcserezli@gmail.com>
2023-10-15 12:20:14 +0200

    Fix comment typo in Component.hpp

--- BoilingHotWater/BHW/ecs/Component.hpp
+++ BoilingHotWater/BHW/ecs/Component.hpp
@@ -10,7 +10,7 @@ namespace BHW
     // changes undeterministically at runtime | determines the index of a component
     typedef uint64_t ComponentIndex;
 
-    // combines of ComponentTypeUUID and ComponentIndex | (ComponentTypeUUID << 32) | (ComponentIndex)
+    // combines ComponentTypeUUID and ComponentIndex | (ComponentTypeUUID << 32) | (ComponentIndex)
     typedef uint64_t ComponentUUID;
 
     template <typename ...TComponents>
