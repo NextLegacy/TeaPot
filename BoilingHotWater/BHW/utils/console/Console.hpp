63eb0541b5cb2ae1acf81473d9dacca758931e2a
NextLegacy <mcserezli@gmail.com>
2023-10-16 12:21:44 +0200

    Refactor Console.hpp to add a formatted WriteLine function

--- BoilingHotWater/BHW/utils/console/Console.hpp
+++ BoilingHotWater/BHW/utils/console/Console.hpp
@@ -7,17 +7,24 @@
 #endif
 
 #include <iostream>
-#include <string>
+
+#include "BHW/utils/String.hpp"
 
 namespace BHW
 {
     namespace Console
     {
-        void InitConsole    ();
-        void ShutdownConsole();
+        void Initialize();
+        void Shutdown  ();
 
         void Write    (const std::string& message);
         void WriteLine(const std::string& message);
+
+        template<typename... TArgs>
+        void WriteLineFormatted(const std::string& format, TArgs&&... messages)
+        {
+            WriteLine(BHW::Format(format, std::forward<TArgs>(messages)...));
+        }
 
         void Pause();
 
