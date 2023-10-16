9e18844259c022a21f5f6419b295bec2f2ace454
NextLegacy <mcserezli@gmail.com>
2023-10-16 12:21:23 +0200

    Add ToString and Format functions to String.hpp

--- BoilingHotWater/BHW/utils/String.hpp
+++ BoilingHotWater/BHW/utils/String.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
 #include <string>
+#include <format>
 
 namespace BHW
 {
-    inline std::string to_string(const float& f);
+    std::string ToString(const float& f);
+
+    template<typename... TArgs>
+    std::string Format(const std::string& format, TArgs&&... messages)
+    {
+        return std::vformat(format, std::make_format_args(messages...));
+    }
 }
