ed3048add6c7c673cb2e2c38f5121afd96b2c824
NextLegacy <mcserezli@gmail.com>
2023-10-18 12:23:19 +0200

    Add JSONType enum to BHW utils json

---
+++ BoilingHotWater/BHW/utils/json/JSONType.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace BHW
+{
+    enum class JSONType
+    {
+        String,
+        Int64 ,
+        Int32 ,
+        Int16 ,
+        Int8  ,
+        UInt64,
+        UInt32,
+        UInt16,
+        UInt8 ,
+        Float ,
+        Double,
+        Bool  ,
+        Array ,
+        Object,
+        Null
+    };
+}
