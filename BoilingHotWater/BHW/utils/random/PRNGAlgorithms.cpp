d63d2abf9c1b8ec7e0b6c7cffb0d9a1ec0fb9225
NextLegacy <mcserezli@gmail.com>
2023-10-18 12:23:40 +0200

    Refactor PRNGAlgorithms.cpp for constexpr functions

--- BoilingHotWater/BHW/utils/random/PRNGAlgorithms.cpp
+++ BoilingHotWater/BHW/utils/random/PRNGAlgorithms.cpp
@@ -4,7 +4,7 @@ namespace BHW
 {
     namespace PRNGAlgorithms
     {
-        uint64_t Splitmix64(uint64_t &state)
+        constexpr uint64_t Splitmix64(uint64_t &state)
         {
             state += 0x9E3779B97F4A7C15UL;
 
@@ -16,8 +16,9 @@ namespace BHW
             return random ^ (random >> 31);
         }
 
-        uint64_t Xoshiro256ss(uint64_t (&state)[4])
+        constexpr uint64_t Xoshiro256ss(uint64_t (&state)[4])
         {
+            /*
             uint64_t* s = state;
             uint64_t const result = s[0] + s[3];
 
@@ -31,6 +32,20 @@ namespace BHW
             s[2] ^= t;
 
             s[3] = (s[3] << 45) | (s[3] >> (64 - 45)); // rotl(s[3], 45);
+            */
+
+            uint64_t const result = state[0] + state[3];
+
+            uint64_t const t = state[1] << 17;
+
+            state[2] ^= state[0];
+            state[3] ^= state[1];
+            state[1] ^= state[2];
+            state[0] ^= state[3];
+
+            state[2] ^= t;
+
+            state[3] = (state[3] << 45) | (state[3] >> (64 - 45)); // rotl(state[3], 45);
 
             return result;
         }
