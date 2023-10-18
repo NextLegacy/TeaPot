3d685e20a21aa395512c88b06168b7fb74bf5d8f
NextLegacy <mcserezli@gmail.com>
2023-10-18 12:23:49 +0200

    Refactor random number generators in BHW utils

--- BoilingHotWater/BHW/utils/random/Random.cpp
+++ BoilingHotWater/BHW/utils/random/Random.cpp
@@ -2,23 +2,18 @@
 
 namespace BHW
 {
-    void Splitmix64::InitState()
-    {
-        m_state = m_seed;
-    }
+    Splitmix64::Splitmix64(         ) : Prng(    ) { InitState(); }
+    Splitmix64::Splitmix64(Seed seed) : Prng(seed) { InitState(); }
 
-    uint64_t Splitmix64::operator()()
-    {
-        return PRNGAlgorithms::Splitmix64(m_state);
-    }
+    void Splitmix64::InitState() { m_state = m_seed; }
 
-    void Xoshiro256ss::InitState()
-    {
-        m_seed.CreateState(m_state);
-    }
+    uint64_t Splitmix64::operator()() { return PRNGAlgorithms::Splitmix64(m_state); }
 
-    uint64_t Xoshiro256ss::operator()()
-    {
-        return PRNGAlgorithms::Xoshiro256ss(m_state);
-    }
+
+    Xoshiro256ss::Xoshiro256ss(         ) : Prng(    ) { InitState(); }
+    Xoshiro256ss::Xoshiro256ss(Seed seed) : Prng(seed) { InitState(); }
+
+    void Xoshiro256ss::InitState() { m_seed.CreateState(m_state); }
+    
+    uint64_t Xoshiro256ss::operator()() { return PRNGAlgorithms::Xoshiro256ss(m_state); }
 }
