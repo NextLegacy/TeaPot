61ea08d8315c3a89f6c459de35f059da41ce6c8b
NextLegacy <mcserezli@gmail.com>
2023-10-20 12:25:12 +0200

    Update TeaCupBaseEventSystem to include TApplication parameter in Start, Update, FixedUpdate, and Render methods

--- TeaCup/TC/TeaCupBaseEventSystem.hpp
+++ TeaCup/TC/TeaCupBaseEventSystem.hpp
@@ -7,14 +7,15 @@
 
 namespace TC
 {
+    template <typename TApplication>
     struct TeaCupBaseEvent
     {
-        inline virtual void Start      () { }
+        inline virtual void Start      (TApplication& application) { }
 
-        inline virtual void Update     () { }
-        inline virtual void FixedUpdate() { }
-        inline virtual void Render     () { }
+        inline virtual void Update     (TApplication& application) { }
+        inline virtual void FixedUpdate(TApplication& application) { }
+        inline virtual void Render     (TApplication& application) { }
 
-        inline virtual void End        () { }
+        inline virtual void End        (TApplication& application) { }
     };
 }
