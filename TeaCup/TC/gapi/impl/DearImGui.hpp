fbb03332f816c54577018d0911b0ae47b4de5a1a
NextLegacy <mcserezli@gmail.com>
2023-10-20 12:25:29 +0200

    Refactor DearImGui class and remove unused code

--- TeaCup/TC/gapi/impl/DearImGui.hpp
+++ TeaCup/TC/gapi/impl/DearImGui.hpp
@@ -1,58 +1,28 @@
 #pragma once
 
 #include <imgui.h>
+#include <imgui_internal.h>
 
 #include "TC/gapi/GAPI.hpp"
 
-#include "TC/gapi/impl/ImGui/ImGuiWindow.hpp"
-
 namespace TC
 {
-    template <typename ...TWindows>
     class DearImGui
     {
     public:
-        inline DearImGui() : m_imguiContext(nullptr), m_imguiIO(nullptr), m_imguiStyle(nullptr), m_windows() { }
+        inline DearImGui() : m_imguiContext(nullptr), m_imguiIO(nullptr), m_imguiStyle(nullptr) { }
 
-        void RenderImGuiWindows()
-        {
-            std::apply([&](auto&... windows)
-            {
-                ((std::for_each(windows.begin(), windows.end(), [](auto window)
-                {
-                    window->Render();
-                })), ...);
-            }, m_windows);
+        inline std::string CreateUniqueName(const std::string& name) const 
+        { 
+            static uint32_t m_id = 0;
+            return name + "###" + std::to_string(m_id++); 
         }
 
-        template <typename TWindow, typename ...TArgs>
-        TWindow& AddImGuiWindow(TArgs&&... args)
-        {
-            static_assert(std::disjunction_v<std::is_same<TWindow, TWindows>...>, "DearImGui: Window type not supported");
-
-            BHW::Console::WriteLine("DearImGui: Adding window of type " + std::string(typeid(TWindow).name()));
-
-            TWindow* window = new TWindow(std::forward<TArgs>(args)...);
-
-            std::get<std::vector<TWindow*>>(m_windows).push_back(window);
-
-            return *window;
-        }
-
-        void RemoveImGuiWindow(const std::string& name) 
-        {
-            std::apply([&](auto&... windows)
-            {
-                ((windows.erase(std::remove_if(windows.begin(), windows.end(), [&](auto window)
-                {
-                    return window->GetName() == name;
-                }))), ...);
-            }, m_windows);
-        }
+        inline ImGuiContext* GetContext() const { return m_imguiContext; }
+        inline ImGuiIO*      GetIO     () const { return m_imguiIO;      }
+        inline ImGuiStyle*   GetStyle  () const { return m_imguiStyle;   }
 
     protected:
-        std::tuple<std::vector<TWindows*>...> m_windows;
-
         ImGuiContext* m_imguiContext;
         ImGuiIO*      m_imguiIO;
         ImGuiStyle*   m_imguiStyle;
