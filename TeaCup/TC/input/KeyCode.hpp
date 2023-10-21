205f94b4028b6fc16075ccb99117c32589edf2c4
NextLegacy <mcserezli@gmail.com>
2023-10-21 12:26:24 +0200

    Add include for <map> in KeyCode.hpp

--- TeaCup/TC/input/KeyCode.hpp
+++ TeaCup/TC/input/KeyCode.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <map>
 
 namespace TC
 {
@@ -11,50 +12,50 @@ namespace TC
 
     enum class KeyCode
     {
-        Space               = 32,
+        Space               = 32 ,
         Apostrophe          = 39 ,
         Comma               = 44 ,
         Minus               = 45 ,
         Period              = 46 ,
         Slash               = 47 ,
-        Num0                = 48,
-        Num1                = 49,
-        Num2                = 50,
-        Num3                = 51,
-        Num4                = 52,
-        Num5                = 53,
-        Num6                = 54,
-        Num7                = 55,
-        Num8                = 56,
-        Num9                = 57,
+        Num0                = 48 ,
+        Num1                = 49 ,
+        Num2                = 50 ,
+        Num3                = 51 ,
+        Num4                = 52 ,
+        Num5                = 53 ,
+        Num6                = 54 ,
+        Num7                = 55 ,
+        Num8                = 56 ,
+        Num9                = 57 ,
         Semicolon           = 59 ,
         Equal               = 61 ,
-        A                   = 65,
-        B                   = 66,
-        C                   = 67,
-        D                   = 68,
-        E                   = 69,
-        F                   = 70,
-        G                   = 71,
-        H                   = 72,
-        I                   = 73,
-        J                   = 74,
-        K                   = 75,
-        L                   = 76,
-        M                   = 77,
-        N                   = 78,
-        O                   = 79,
-        P                   = 80,
-        Q                   = 81,
-        R                   = 82,
-        S                   = 83,
-        T                   = 84,
-        U                   = 85,
-        V                   = 86,
-        W                   = 87,
-        X                   = 88,
-        Y                   = 89,
-        Z                   = 90,
+        A                   = 65 ,
+        B                   = 66 ,
+        C                   = 67 ,
+        D                   = 68 ,
+        E                   = 69 ,
+        F                   = 70 ,
+        G                   = 71 ,
+        H                   = 72 ,
+        I                   = 73 ,
+        J                   = 74 ,
+        K                   = 75 ,
+        L                   = 76 ,
+        M                   = 77 ,
+        N                   = 78 ,
+        O                   = 79 ,
+        P                   = 80 ,
+        Q                   = 81 ,
+        R                   = 82 ,
+        S                   = 83 ,
+        T                   = 84 ,
+        U                   = 85 ,
+        V                   = 86 ,
+        W                   = 87 ,
+        X                   = 88 ,
+        Y                   = 89 ,
+        Z                   = 90 ,
         LeftBracket         = 91 ,
         Backslash           = 92 ,
         RightBracket        = 93 ,
@@ -131,7 +132,7 @@ namespace TC
         RightAlt            = 346,
         RightSuper          = 347,
         Menu                = 348,
-        Last                = 348
+        Last                = 348,
     };
 
     enum class ModifierCode
@@ -141,14 +142,14 @@ namespace TC
         Alt                 = 0x0004,
         Super               = 0x0008,
         CapsLock            = 0x0010,
-        NumLock             = 0x0020
+        NumLock             = 0x0020,
     };
 
     enum class ActionCode
     {
         Release             = 0,
         Press               = 1,
-        Repeat              = 2
+        Repeat              = 2,
     };
 
     enum class MouseButtonCode
@@ -165,6 +166,21 @@ namespace TC
         Right               = 1,
         Wheel               = 2,
     };
+
+    extern std::map<KeyCode        , std::string> KeyCodeToStringMap        ;
+    extern std::map<ModifierCode   , std::string> ModifierCodeToStringMap   ;
+    extern std::map<ActionCode     , std::string> ActionCodeToStringMap     ;
+    extern std::map<MouseButtonCode, std::string> MouseButtonCodeToStringMap;
+
+    // TODO: maybe use templates here?
+    // something like template<typename T> std::string ToString(T);
+
+    extern std::string ToString(KeyCode         keyCode     );
+    extern std::string ToString(ModifierCode    modifierCode);
+    extern std::string ToString(ActionCode      actionCode  );
+    extern std::string ToString(MouseButtonCode mouseCode   );
+
+    // TODO: support for other input devices
 
     /*
     enum class CursorCode
@@ -233,11 +249,6 @@ namespace TC
         GamepadRightTrigger = GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER
     };
     */
-
-    extern std::string ToString(KeyCode         keyCode     );
-    extern std::string ToString(ModifierCode    modifierCode);
-    extern std::string ToString(ActionCode      actionCode  );
-    extern std::string ToString(MouseButtonCode mouseCode   );
 
     /*
     std::string ToString(CursorCode      cursorCode     );
