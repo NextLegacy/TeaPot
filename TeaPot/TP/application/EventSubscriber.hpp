88e5310c89c7df830b5a3d055c46902c461b6ac3
NextLegacy <mcserezli@gmail.com>
2023-10-22 12:26:58 +0200

    Refactor EventSubscriber struct in TeaPot application

--- TeaPot/TP/application/EventSubscriber.hpp
+++ TeaPot/TP/application/EventSubscriber.hpp
@@ -8,11 +8,5 @@ namespace TP
 {
     struct TeaPot;
 
-    struct EventSubscriber : public TC::TeaCupBaseEvent
-    {
-        void Initialize(std::shared_ptr<TeaPot> teaPot);
-
-    protected:
-        std::shared_ptr<TeaPot> m_teaPot;
-    };
+    struct EventSubscriber : public TC::TeaCupBaseEvent<TeaPot> { };
 }
