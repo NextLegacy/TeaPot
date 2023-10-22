12350929d27350a8101d8a4437584984452a45dd
NextLegacy <mcserezli@gmail.com>
2023-10-22 12:27:18 +0200

    Add TeaPotProject and update DebugSystem

--- TeaPot/TP/application/ecs/systems/DebugSystem.cpp
+++ TeaPot/TP/application/ecs/systems/DebugSystem.cpp
@@ -2,10 +2,25 @@
 
 #include "TP/application/TeaPot.hpp"
 
+#include "TP/project/TeaPotProject.hpp"
+
 namespace TP
 {
-    void DebugSystem::Update()
+    void DebugSystem::Start(TeaPot& teaPot)
     {
-        BHW::Console::WriteLine("FPS: " + std::to_string(m_teaPot->GetFPS()));
+        TeaPotProject project = LoadOrCreateProject("C:/Users/mertc/Desktop/Test/TestProject");
+
+        BHW::Console::WriteLine("Project name: " + project.m_projectName);
+        BHW::Console::WriteLine("Project number: " + std::to_string(project.m_someNumber));
     }
+
+    void DebugSystem::Update(TeaPot& teaPot)
+    {
+        //BHW::Console::WriteLine("FPS: " + std::to_string(teaPot.GetFPS()));
+    }
+
+    void DebugSystem::Render(TeaPot& teaPot)
+    {
+        //ImGui::ShowDemoWindow();
+    }   
 }
