c859a4f41968f38831c1381a5ef5d54fd5d0cf84
NextLegacy <mcserezli@gmail.com>
2023-10-22 12:27:03 +0200

    Add ECSEventSystemSubscriber functions to TeaPot.cpp

--- TeaPot/TP/application/TeaPot.cpp
+++ TeaPot/TP/application/TeaPot.cpp
@@ -2,4 +2,9 @@
 
 namespace TP
 {
+    void ECSEventSystemSubscriber::Start      (TeaPot& teaPot) { static_cast<ECS&>(teaPot).ForEachEventSystem(&EventSubscriber::Start      , teaPot); }
+    void ECSEventSystemSubscriber::Update     (TeaPot& teaPot) { static_cast<ECS&>(teaPot).ForEachEventSystem(&EventSubscriber::Update     , teaPot); }
+    void ECSEventSystemSubscriber::FixedUpdate(TeaPot& teaPot) { static_cast<ECS&>(teaPot).ForEachEventSystem(&EventSubscriber::FixedUpdate, teaPot); }
+    void ECSEventSystemSubscriber::Render     (TeaPot& teaPot) { static_cast<ECS&>(teaPot).ForEachEventSystem(&EventSubscriber::Render     , teaPot); }
+    void ECSEventSystemSubscriber::End        (TeaPot& teaPot) { static_cast<ECS&>(teaPot).ForEachEventSystem(&EventSubscriber::End        , teaPot); }
 }
