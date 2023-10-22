2b2814bfc19bc515aa09158fc284c92cb4c23a77
NextLegacy <mcserezli@gmail.com>
2023-10-22 12:27:07 +0200

    Add GUI components and renderers

--- TeaPot/TP/application/TeaPot.hpp
+++ TeaPot/TP/application/TeaPot.hpp
@@ -3,6 +3,7 @@
 #include <BHW/utils/console/Console.hpp>
 #include <BHW/ecs/ECS.hpp>
 #include <BHW/utils/event/EventSystem.hpp>
+#include <BHW/utils/random/Random.hpp>
 
 #include <TC/TeaCup.hpp>
 #include <TC/gapi/impl/GLFW_OpenGL_DearImGui.hpp>
@@ -12,41 +13,59 @@
 #include "TP/application/ecs/components/DebugComponent.hpp"
 #include "TP/application/ecs/systems/DebugSystem.hpp"
 
-#include "TP/application/windows/ConsoleWindow.hpp"
-#include "TP/application/windows/Dockspace.hpp"
+#include "TP/application/gui/window/ConsoleWindow.hpp"
+#include "TP/application/gui/Dockspace.hpp"
+#include "TP/application/gui/MenuBar.hpp"
 
 namespace TP
 {
-    typedef TC::TeaCup<TC::GLFW_OpenGL_DearImGui<
-        Dockspace,
-        ConsoleWindow
-    >, EventSubscriber> TeaCup;
+    class TeaPot;
+
+    typedef TC::TeaCup<TC::GLFW_OpenGL_DearImGui, EventSubscriber, TeaPot> TeaCup;
 
     typedef BHW::EventSystem<EventSubscriber, 
-        DebugSystem
+        DebugSystem,
+        DockspaceRenderer,
+        ConsoleWindowRenderer,
+        MenuBarRenderer
     > EventSystem;
 
     typedef BHW::ECS<EventSystem,
-        DebugComponent
+        DebugComponent,
+        DockspaceData,
+        ConsoleWindowData
     > ECS;
 
-    class TeaPot 
-        : public std::enable_shared_from_this<TeaPot>,
-          public TeaCup,
-          public ECS
+    class ECSEventSystemSubscriber : public EventSubscriber
+    {
+        virtual void Start      (TeaPot& teaPot) override;
+        virtual void Update     (TeaPot& teaPot) override;
+        virtual void FixedUpdate(TeaPot& teaPot) override;
+        virtual void Render     (TeaPot& teaPot) override;
+        virtual void End        (TeaPot& teaPot) override;
+    };
+
+    class TeaPot : public TeaCup, public ECS
     {
     public:
-        inline TeaPot() : TeaCup(), ECS()
+        inline TeaPot() : TeaCup(), ECS(), m_random()
         {
-            AddImGuiWindow<TP::Dockspace>(*this);
+            SubscribeEventSystem<ECSEventSystemSubscriber>();
+
+            GetEntity(CreateEntity()).AddComponent<DockspaceData>("Dockspace");
         }
 
-        template <typename ...TArgs>
-        inline void ForEachEventSystem(void (EventSubscriber::*function)(TArgs...), TArgs... args)
+        template <typename TWindow, typename... TArgs>
+        inline BHW::Entity CreateWindow(TArgs&&... args)
         {
-            BHW::Console::WriteLine("TeaPot::ForEachEventSystem");
-            TC::TeaCup::ForEachEventSystem(function, args...);
-            ECS::ForEachEventSystem(function, args...);
+            auto entity = GetEntity(CreateEntity());
+            entity.AddComponent<TWindow>(std::forward<TArgs>(args)...);
+            return entity;
         }
+
+    private:
+        friend class ECSEventSystemSubscriber;
+
+        BHW::Random m_random;
     };
 }
