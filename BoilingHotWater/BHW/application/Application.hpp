df3184d343c202b938ef513d64f9b62842c60fda
NextLegacy <mcserezli@gmail.com>
2023-11-02 13:23:41 +0100

    Remove destructor and add Stop() function to Application class

--- BoilingHotWater/BHW/application/Application.hpp
+++ BoilingHotWater/BHW/application/Application.hpp
@@ -1,16 +1,14 @@
 #pragma once
 
-#include <type_traits>
-
 namespace BHW
 {
     class Application
     {
     public:
         Application();
-        ~Application();
 
         void Run();
+        void Stop();
 
         inline bool IsRunning() { return m_isRunning; }
 
@@ -20,31 +18,4 @@ namespace BHW
     private:
         bool m_isRunning;
     };
-
-    namespace
-    {
-        extern Application* s_instance;
-    }
-
-    template <typename TApplication>
-    TApplication& GetApplication()
-    {
-        return static_cast<TApplication&>(Application::GetInstance());
-    }
-
-    template <typename TApplication>
-    TApplication CreateApplication()
-    {
-        TApplication application();
-
-        return GetApplication<TApplication>();
-    }
-
-    template <typename TApplication>
-    TApplication& CreateAndRunApplication()
-    {
-        CreateApplication<TApplication>().Run();
-
-        return GetApplication<TApplication>();
-    }
 }
