cd35da92c366cf9d7ba7c7b5c075e4e107c493ae
NextLegacy <mcserezli@gmail.com>
2023-11-02 13:24:11 +0100

    Add Windows-specific file handling functions

--- BoilingHotWater/BHW/utils/io/FileHandler.hpp
+++ BoilingHotWater/BHW/utils/io/FileHandler.hpp
@@ -2,6 +2,13 @@
 
 #include <string>
 
+#ifdef _WIN32
+#include <windows.h>
+#include <shobjidl.h>
+#include <shobjidl_core.h>
+#include <comdef.h>
+#endif
+
 namespace BHW
 {
     std::string ReadFile(const std::string& path);
@@ -9,4 +16,9 @@ namespace BHW
     void WriteFile(const std::string& path, const std::string& content);
 
     bool FileExists(const std::string& path);
+
+    void CreateFolder(const std::string& path);
+    void CreateFile(const std::string& path);
+
+    std::string SelectFolder(const std::string& title, const std::string& defaultPath, HWND parent);
 }
