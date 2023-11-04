5c3b05315a2b059af2617621fd11f03af1fc4e46
NextLegacy <mcserezli@gmail.com>
2023-11-04 13:24:40 +0100

    Add GetJSONObject() method to Serializable class

--- BoilingHotWater/BHW/utils/json/Serializable.hpp
+++ BoilingHotWater/BHW/utils/json/Serializable.hpp
@@ -11,6 +11,8 @@ namespace BHW
 
         std::string Serialize  (                      );
         void        Deserialize(std::string jsonString);
+
+        JSONObject& GetJSONObject() { return m_jsonObject; }
     
     protected:
         void Register(std::string key, JSONObject jsonObject);
