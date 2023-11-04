8a1452152a09ef29ffbd1278288abed326d3c053
NextLegacy <mcserezli@gmail.com>
2023-11-04 13:25:01 +0100

    Add reflection support for primitive types and std::string

---
+++ BoilingHotWater/BHW/utils/reflection/Reflection.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "BHW/utils/reflection/Member.hpp"
+#include "BHW/utils/reflection/ReflectionDefinitions.hpp"
+
+BHW_REFLECTION_TYPE(bool              );
+BHW_REFLECTION_TYPE(char              );
+BHW_REFLECTION_TYPE(unsigned char     );
+BHW_REFLECTION_TYPE(short             );
+BHW_REFLECTION_TYPE(unsigned short    );
+BHW_REFLECTION_TYPE(int               );
+BHW_REFLECTION_TYPE(unsigned int      );
+BHW_REFLECTION_TYPE(long              );
+BHW_REFLECTION_TYPE(unsigned long     );
+BHW_REFLECTION_TYPE(long long         );
+BHW_REFLECTION_TYPE(unsigned long long);
+BHW_REFLECTION_TYPE(float             );
+BHW_REFLECTION_TYPE(double            );
+BHW_REFLECTION_TYPE(long double       );
+BHW_REFLECTION_TYPE(std::string       );
+BHW_REFLECTION_TYPE(std::string_view  );
