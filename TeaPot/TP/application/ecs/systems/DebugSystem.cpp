d3ccbb105435726c30aa0efca9951521ec6063fa
NextLegacy <mcserezli@gmail.com>
2023-11-12 13:29:51 +0100

    Remove unused code in DebugSystem.cpp

--- TeaPot/TP/application/ecs/systems/DebugSystem.cpp
+++ TeaPot/TP/application/ecs/systems/DebugSystem.cpp
@@ -2,25 +2,17 @@
 
 #include "TP/application/TeaPot.hpp"
 
-#include "TP/project/TeaPotProject.hpp"
-
 namespace TP
 {
     void DebugSystem::Start(TeaPot& teaPot)
     {
-        TeaPotProject project = LoadOrCreateProject("C:/Users/mertc/Desktop/Test/TestProject");
-
-        BHW::Console::WriteLine("Project name: " + project.m_projectName);
-        BHW::Console::WriteLine("Project number: " + std::to_string(project.m_someNumber));
     }
 
     void DebugSystem::Update(TeaPot& teaPot)
     {
-        //BHW::Console::WriteLine("FPS: " + std::to_string(teaPot.GetFPS()));
     }
 
     void DebugSystem::Render(TeaPot& teaPot)
     {
-        //ImGui::ShowDemoWindow();
     }   
 }
