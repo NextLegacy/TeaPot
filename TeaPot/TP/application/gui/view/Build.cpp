b181c8b713bbf74bef7ae92f5e225205be48f20c
NextLegacy <mcserezli@gmail.com>
2023-11-13 13:30:34 +0100

    Add Build.cpp for GUI view rendering

---
+++ TeaPot/TP/application/gui/view/Build.cpp
@@ -0,0 +1,30 @@
+#include "TP/application/gui/view/Build.hpp"
+
+#include "TP/application/TeaPot.hpp"
+
+namespace TP
+{
+    namespace View
+    {
+        Build::Build(std::string name, bool open) : m_name(name), m_open(open) { }
+
+        void BuildRenderer::Render(TeaPot& teaPot)
+        {
+            teaPot.ForEachComponent<Build>([&](BHW::EntityUUID uuid, Build& data)
+            {
+                RenderBuild(teaPot, data);
+            });
+        }
+
+        void BuildRenderer::RenderBuild(TeaPot& teaPot, Build& data)
+        {
+            if (!data.m_open) return;
+
+            ImGui::Begin(data.m_name.c_str(), &data.m_open);
+
+            ImGui::Text("Build");
+            
+            ImGui::End();
+        }
+    }
+}
