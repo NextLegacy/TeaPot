10ecd1e4ba97212c9c576056aeb29dcb207e2d31
NextLegacy <mcserezli@gmail.com>
2023-11-13 13:30:40 +0100

    Add Build.hpp and BuildRenderer class to TeaPot/TP/application/gui/view

---
+++ TeaPot/TP/application/gui/view/Build.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "TP/application/EventSubscriber.hpp"
+
+namespace TP
+{
+    namespace View
+    {
+        struct Build
+        {
+            std::string m_name;
+            bool        m_open;
+
+            Build(std::string name, bool open = true);
+        };
+
+        class BuildRenderer : public EventSubscriber
+        {
+        public:
+            void Render(TeaPot& teaPot);
+
+        private:
+            void RenderBuild(TeaPot& teaPot, Build& data);
+        };
+    }
+}
