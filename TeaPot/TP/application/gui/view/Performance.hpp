9ceea2ab285627a01b680bf00506bcffcf0f2a71
NextLegacy <mcserezli@gmail.com>
2023-11-14 13:31:39 +0100

    Add Performance.hpp for TeaPot GUI view

---
+++ TeaPot/TP/application/gui/view/Performance.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "TP/application/EventSubscriber.hpp"
+
+namespace TP
+{
+    namespace View
+    {
+        struct Performance
+        {
+            std::string m_name;
+            bool        m_open;
+
+            Performance(std::string name, bool open = true);
+        };
+
+        class PerformanceRenderer : public EventSubscriber
+        {
+        public:
+            void Render(TeaPot& teaPot);
+
+        private:
+            void RenderPerformance(TeaPot& teaPot, Performance& data);
+        };
+    }
+}
