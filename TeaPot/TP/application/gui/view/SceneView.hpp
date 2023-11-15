e1e7474424ca2719b0860b1529b378e62611e20e
NextLegacy <mcserezli@gmail.com>
2023-11-15 13:38:22 +0100

    Add SceneView and SceneViewRenderer classes

---
+++ TeaPot/TP/application/gui/view/SceneView.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "TP/application/EventSubscriber.hpp"
+
+namespace TP
+{
+    namespace View
+    {
+        struct SceneView
+        {
+            std::string m_name;
+            bool        m_open;
+
+            SceneView(std::string name, bool open = true);
+        };
+
+        class SceneViewRenderer : public EventSubscriber
+        {
+        public:
+            void Render(TeaPot& teaPot);
+
+        private:
+            void RenderSceneView(TeaPot& teaPot, SceneView& data);
+        };
+    }
+}
