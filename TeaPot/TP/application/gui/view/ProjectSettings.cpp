ef71cc4cdfdca9c14c3ba55038eb1bc28559ca72
NextLegacy <mcserezli@gmail.com>
2023-11-15 13:37:57 +0100

    Add ProjectSettings view to TeaPot application

---
+++ TeaPot/TP/application/gui/view/ProjectSettings.cpp
@@ -0,0 +1,30 @@
+#include "TP/application/gui/view/ProjectSettings.hpp"
+
+#include "TP/application/TeaPot.hpp"
+
+namespace TP
+{
+    namespace View
+    {
+        ProjectSettings::ProjectSettings(std::string name, bool open) : m_name(name), m_open(open) { }
+
+        void ProjectSettingsRenderer::Render(TeaPot& teaPot)
+        {
+            teaPot.ForEachComponent<ProjectSettings>([&](BHW::EntityUUID uuid, ProjectSettings& data)
+            {
+                RenderProjectSettings(teaPot, data);
+            });
+        }
+
+        void ProjectSettingsRenderer::RenderProjectSettings(TeaPot& teaPot, ProjectSettings& data)
+        {
+            if (!data.m_open) return;
+
+            ImGui::Begin(data.m_name.c_str(), &data.m_open);
+
+            ImGui::Text("Project Settings");
+            
+            ImGui::End();
+        }
+    }
+}
