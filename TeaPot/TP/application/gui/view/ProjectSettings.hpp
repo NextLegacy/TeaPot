11384cbc2c16fd78df057212396fe7135fbc68c2
NextLegacy <mcserezli@gmail.com>
2023-11-15 13:38:02 +0100

    Add ProjectSettings and ProjectSettingsRenderer classes

---
+++ TeaPot/TP/application/gui/view/ProjectSettings.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "TP/application/EventSubscriber.hpp"
+
+namespace TP
+{
+    namespace View
+    {
+        struct ProjectSettings
+        {
+            std::string m_name;
+            bool        m_open;
+
+            ProjectSettings(std::string name, bool open = true);
+        };
+
+        class ProjectSettingsRenderer : public EventSubscriber
+        {
+        public:
+            void Render(TeaPot& teaPot);
+
+        private:
+            void RenderProjectSettings(TeaPot& teaPot, ProjectSettings& data);
+        };
+    }
+}
