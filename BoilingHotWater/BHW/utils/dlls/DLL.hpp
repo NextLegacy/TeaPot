26e97e1da69610c82f830724314b0acac4b01c2d
NextLegacy <mcserezli@gmail.com>
2023-11-23 13:51:31 +0100

    Add DLL utility class

---
+++ BoilingHotWater/BHW/utils/dlls/DLL.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#define BHW_DLL_EXPORT __declspec(dllexport)
+#define BHW_DLL_IMPORT __declspec(dllimport)
+
+#include <Windows.h>
+
+namespace BHW
+{
+    class DLL
+    {
+    public:
+        DLL(const char* path)
+        {
+            m_handle = LoadLibraryA(path);
+        }
+
+        ~DLL()
+        {
+            FreeLibrary(m_handle);
+        }
+
+        template<typename TFunction>
+        TFunction GetFunction(const char* name)
+        {
+            return reinterpret_cast<T>(GetProcAddress(m_handle, name));
+        }
+
+    private:
+        HMODULE m_handle;
+    };
+}
