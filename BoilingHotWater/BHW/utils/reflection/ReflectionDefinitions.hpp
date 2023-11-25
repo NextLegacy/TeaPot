58352d0736269d78084385e50ccfb432551891d5
NextLegacy <mcserezli@gmail.com>
2023-11-25 13:52:45 +0100

    Refactor reflection macros and add type casting

--- BoilingHotWater/BHW/utils/reflection/ReflectionDefinitions.hpp
+++ BoilingHotWater/BHW/utils/reflection/ReflectionDefinitions.hpp
@@ -3,18 +3,25 @@
 #include "BHW/utils/reflection/Type.hpp"
 #include "BHW/utils/reflection/TypeInfo.hpp"
 
-#define BHW_REFLECTION_CONCAT(a, b) a##b
-#define BHW_REFLECTION_TYPE_INTERNAL(class_name, var_name, ...) \
+// TODO: move this to a separate file
+#define BHW_CONCAT(a, b) a##b
+
+#define BHW_REFLECT_INTERNAL(class_name, var_name, ...) \
     namespace BHW \
     { \
         namespace __reflection \
         { \
-            inline static const TypeInfo& BHW_REFLECTION_CONCAT(__, var_name) = MakeTypeInfo<class_name, __VA_ARGS__>(); \
+            inline static const TypeInfo& BHW_CONCAT(__, var_name) = MakeTypeInfo<class_name, __VA_ARGS__>(); \
         } \
         template <> \
-        inline constexpr const TypeInfo& GetTypeInfo<Hash<class_name>()>() \
+        inline constexpr auto Cast<TypeHash<class_name>()>(void* ptr) \
         { \
-            return __reflection::BHW_REFLECTION_CONCAT(__, var_name); \
+            return static_cast<class_name*>(ptr); \
+        } \
+        template <> \
+        inline constexpr const TypeInfo& GetTypeInfo<TypeHash<class_name>()>() \
+        { \
+            return __reflection::BHW_CONCAT(__, var_name); \
         } \
         template <> \
         inline constexpr bool IsRegistered<class_name>() \
@@ -23,20 +30,22 @@
         } \
     }
 
-#define BHW_REFLECTION_TYPE(class_name, ...) BHW_REFLECTION_TYPE_INTERNAL(class_name, __COUNTER__, __VA_ARGS__)
+#define BHW_REFLECT(class_name, ...) BHW_REFLECT_INTERNAL(class_name, __COUNTER__, __VA_ARGS__)
 
-#define BHW_REFLECTION_MEMBERS(class_name, ...) \
+#define BHW_REFLECT_MEMBERS_INTERNAL(class_name, var_name, ...) \
     namespace BHW \
     { \
         namespace __reflection \
         { \
-            inline static const auto __##class_name##_members = MakeMembers<class_name>(__VA_ARGS__); \
+            inline static const auto BHW_CONCAT(__, var_name) = MakeMembers<class_name>(__VA_ARGS__); \
         } \
         template <> \
-        inline constexpr auto GetMembers<class_name>() \
+        inline constexpr auto GetMembers<TypeHash<class_name>()>() \
         { \
-            return __reflection::__##class_name##_members; \
+            return __reflection::BHW_CONCAT(__, var_name); \
         } \
     }
 
-#define BHW_REFLECTION_MEMBER(member_name, member_ptr) MakeMember(member_name, member_ptr)
+#define BHW_REFLECT_MEMBERS(class_name, ...) BHW_REFLECT_MEMBERS_INTERNAL(class_name, __COUNTER__, __VA_ARGS__)
+
+#define BHW_REFLECT_MEMBER(member_name, member_ptr) MakeMember(member_name, member_ptr)
