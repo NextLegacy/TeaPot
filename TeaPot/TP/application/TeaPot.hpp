3cdd2afb25e520bdcca3c2133817ff1cfb170f83
NextLegacy <mcserezli@gmail.com>
2023-11-28 14:15:41 +0100

    Refactor TeaPot.hpp to use Tea library

--- TeaPot/TP/application/TeaPot.hpp
+++ TeaPot/TP/application/TeaPot.hpp
@@ -1,14 +1,7 @@
 #pragma once
 
-#include <BHW/utils/console/Console.hpp>
-#include <BHW/ecs/ECS.hpp>
-#include <BHW/utils/event/EventSystem.hpp>
-#include <BHW/utils/random/Random.hpp>
-
-#include <TC/TeaCup.hpp>
 #include <TC/gapi/impl/GLFW_OpenGL_DearImGui.hpp>
-
-#include "TP/application/EventSubscriber.hpp"
+#include <Tea/Tea.hpp>
 
 #include "TP/application/ecs/components/DebugComponent.hpp"
 #include "TP/application/ecs/systems/DebugSystem.hpp"
@@ -31,30 +24,33 @@
 
 namespace TP
 {
-    class TeaPot;
+    class TeaPot : public Tea::Tea
+    <
+        TC::GLFW_OpenGL_DearImGui, 
+        EventSubscriber, 
+        Tea::ECSSystem
+        <
+            EventSubscriber,
+        
+            DebugSystem,
 
-    typedef TC::TeaCup<TC::GLFW_OpenGL_DearImGui, EventSubscriber, TeaPot> TeaCup;
+            DockspaceRenderer,
 
-    typedef BHW::EventSystem<EventSubscriber, 
-        DebugSystem,
+            View::BuildRenderer,
+            View::ConsoleRenderer,
+            View::ExplorerRenderer,
+            View::InspectorRenderer,
+            View::ProjectLoaderRenderer,
+            View::ProjectSettingsRenderer,
+            View::SceneEditorRenderer,
+            View::SceneViewRenderer,
+            View::SettingsRenderer,
+            View::PerformanceRenderer,
 
-        DockspaceRenderer,
+            MenuBarRenderer
+        >,
+        TeaPot,
 
-        View::BuildRenderer,
-        View::ConsoleRenderer,
-        View::ExplorerRenderer,
-        View::InspectorRenderer,
-        View::ProjectLoaderRenderer,
-        View::ProjectSettingsRenderer,
-        View::SceneEditorRenderer,
-        View::SceneViewRenderer,
-        View::SettingsRenderer,
-        View::PerformanceRenderer,
-
-        MenuBarRenderer
-    > EventSystem;
-
-    typedef BHW::ECS<EventSystem,
         DebugComponent,
 
         View::Build,
@@ -67,25 +63,9 @@ namespace TP
         View::SceneView,
         View::Settings,
         View::Performance
-    > ECS;
-
-    class ECSEventSystemSubscriber : public EventSubscriber
-    {
-        virtual void Start      (TeaPot& teaPot) override;
-        virtual void Update     (TeaPot& teaPot) override;
-        virtual void FixedUpdate(TeaPot& teaPot) override;
-        virtual void Render     (TeaPot& teaPot) override;
-        virtual void End        (TeaPot& teaPot) override;
-    };
-
-    class TeaPot : public TeaCup, public ECS
-    {
+    > 
+    { 
     public:
-        inline TeaPot() : TeaCup(), ECS(), m_random()
-        {
-            SubscribeEventSystem<ECSEventSystemSubscriber>();
-        }
-
         template <typename TWindow, typename... TArgs>
         inline TWindow& CreateView(std::string name, TArgs&&... args)
         {
