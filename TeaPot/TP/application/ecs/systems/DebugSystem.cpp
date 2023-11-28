333aa7585a9ad7b680037fe164174ebbe1bed4f2
NextLegacy <mcserezli@gmail.com>
2023-11-28 14:15:45 +0100

    Add JSON and Console includes to DebugSystem.cpp

--- TeaPot/TP/application/ecs/systems/DebugSystem.cpp
+++ TeaPot/TP/application/ecs/systems/DebugSystem.cpp
@@ -1,11 +1,19 @@
 #include "TP/application/ecs/systems/DebugSystem.hpp"
 
+#include <BHW/utils/json/JSON.hpp>
+#include <BHW/utils/console/Console.hpp>
+
 #include "TP/application/TeaPot.hpp"
 
 namespace TP
 {
     void DebugSystem::Start(TeaPot& teaPot)
     {
+        //SomeTest test{ 123, "Hello World", { "Hello", "World" }, { { "Hello", "World" }, { "Hello", "World" } } };
+
+        //BHW::Deserialize<SomeTest>(BHW::Serialize<SomeTest>(test), &test);
+
+        //BHW::Console::WriteLine(BHW::Serialize<SomeTest>(test));
     }
 
     void DebugSystem::Update(TeaPot& teaPot)
