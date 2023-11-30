01c06ba6d9938102f73351c9b351b93b19d82082
NextLegacy <mcserezli@gmail.com>
2023-11-30 20:50:29 +0100

    Refactor Hash.cpp by removing unused code

--- BoilingHotWater/BHW/utils/Hash.cpp
+++ BoilingHotWater/BHW/utils/Hash.cpp
@@ -2,26 +2,5 @@
 
 namespace BHW
 {
-    constexpr unsigned long long Hash(const char* str)
-    {
-        unsigned long long hash = 0xcbf29ce484222325ULL;
-        while (*str)
-        {
-            hash ^= *str;
-            hash *= 0x100000001b3ULL;
-            ++str;
-        }
-        return hash;
-    }
 
-    constexpr unsigned long long Hash(const char* str, unsigned long long length)
-    {
-        unsigned long long hash = 0xcbf29ce484222325ULL;
-        for (unsigned long long i = 0; i < length; ++i)
-        {
-            hash ^= str[i];
-            hash *= 0x100000001b3ULL;
-        }
-        return hash;
-    }
 }
