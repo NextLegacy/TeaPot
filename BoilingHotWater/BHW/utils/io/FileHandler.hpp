3dfbb8208264ddf8208a63e7a03e6d825b3af17d
NextLegacy <mcserezli@gmail.com>
2023-12-01 20:51:01 +0100

    Add file handling utility functions

--- BoilingHotWater/BHW/utils/io/FileHandler.hpp
+++ BoilingHotWater/BHW/utils/io/FileHandler.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #ifdef _WIN32
 #include <windows.h>
 #include <shobjidl.h>
 #include <shobjidl_core.h>
 #include <comdef.h>
+#undef CreateFile
 #endif
 
 namespace BHW
@@ -15,10 +17,30 @@ namespace BHW
 
     void WriteFile(const std::string& path, const std::string& content);
 
+    bool IsFile(const std::string& path);
+
     bool FileExists(const std::string& path);
+    bool PathExists(const std::string& path);
+
+    std::string FileName(const std::string& path);
+    std::string FileNameWithoutExtension(const std::string& path);
+
+    std::string GetFileDirectory(const std::string& path);
 
     void CreateFolder(const std::string& path);
     void CreateFile(const std::string& path);
 
     std::string SelectFolder(const std::string& title, const std::string& defaultPath, HWND parent);
+
+    std::vector<std::string> GetFilesInFolder(const std::string& path, const std::string& extension);
+
+    std::string GetCurrentPath();
+    std::string GetExecutablePath();
+
+    std::string GetAbsolutePath(const std::string& path);
+    bool IsAbsolutePath(const std::string& path);
+
+    bool FileExtensionIs(const std::string& path, const std::string& extension);
+
+    std::string CombinePaths(const std::string& path1, const std::string& path2);
 }
