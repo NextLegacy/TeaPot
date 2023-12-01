6f8f0ea1c4a93d5edaab64d0a1b8173b81710208
NextLegacy <mcserezli@gmail.com>
2023-12-01 20:50:59 +0100

    Refactor file handling functions and add new utility functions

--- BoilingHotWater/BHW/utils/io/FileHandler.cpp
+++ BoilingHotWater/BHW/utils/io/FileHandler.cpp
@@ -19,6 +19,8 @@ namespace BHW
             result += line + "\n";
         }
 
+        file.close();
+
         return result;
     }
 
@@ -27,13 +29,44 @@ namespace BHW
         std::ofstream file(path);
 
         file << content;
+
+        file.close();
+    }
+
+    bool IsFile(const std::string& path)
+    {
+        return std::filesystem::is_regular_file(std::filesystem::path(path));
     }
 
     bool FileExists(const std::string& path)
     {
         std::ifstream file(path);
 
-        return file.good();
+        bool result = file.good();
+
+        file.close();
+
+        return result;
+    }
+
+    bool PathExists(const std::string& path)
+    {
+        return std::filesystem::exists(path);
+    }
+
+    std::string FileName(const std::string& path)
+    {
+        return std::filesystem::path(path).filename().string();
+    }
+
+    std::string FileNameWithoutExtension(const std::string& path)
+    {
+        return std::filesystem::path(path).stem().string();
+    }
+
+    std::string GetFileDirectory(const std::string& path)
+    {
+        return std::filesystem::path(path).parent_path().string();
     }
 
     void CreateFolder(const std::string& path)
@@ -44,13 +77,14 @@ namespace BHW
     void CreateFile(const std::string& path)
     {
         std::ofstream file(path);
+        file.close();
     }
 
     std::string SelectFolder(const std::string& title, const std::string& defaultPath, HWND parent)
     {
 #ifdef _WIN32
 
-        std::wstring folderPath;
+        std::string folderPath;
         IFileOpenDialog* pfd;
 
         HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
@@ -76,7 +110,9 @@ namespace BHW
                         hr = psi->GetDisplayName(SIGDN_FILESYSPATH, &pszPath);
                         if (SUCCEEDED(hr))
                         {
-                            folderPath = pszPath;
+                            int size_needed = WideCharToMultiByte(CP_UTF8, 0, pszPath, -1, NULL, 0, NULL, NULL);
+                            folderPath.resize(size_needed);
+                            WideCharToMultiByte(CP_UTF8, 0, pszPath, -1, &folderPath[0], size_needed, NULL, NULL);
                             CoTaskMemFree(pszPath);
                         }
                         psi->Release();
@@ -91,5 +127,54 @@ namespace BHW
 #else
         return "";
 #endif
+    }
+
+    std::vector<std::string> GetFilesInFolder(const std::string& path, const std::string& extension)
+    {
+        if (!std::filesystem::exists(path)) return std::vector<std::string>();
+
+        std::vector<std::string> files;
+
+        for (const auto& entry : std::filesystem::directory_iterator(path))
+        {
+            if(entry.path().extension() == extension || entry.path().filename() == extension || extension == "*")
+            {
+                files.push_back(entry.path().string());
+            }
+        }
+
+        return files;
+    }
+    
+    std::string GetCurrentPath()
+    {
+        return std::filesystem::current_path().string();
+    }
+
+    std::string GetExecutablePath()
+    {
+        char buffer[MAX_PATH];
+        GetModuleFileNameA(NULL, buffer, sizeof(buffer));
+        return std::filesystem::path(buffer).parent_path().string();
+    }
+
+    std::string GetAbsolutePath(const std::string& path)
+    {
+        return std::filesystem::absolute(path).string();
+    }
+
+    bool IsAbsolutePath(const std::string& path)
+    {
+        return std::filesystem::path(path).is_absolute();
+    }
+
+    bool FileExtensionIs(const std::string& path, const std::string& extension)
+    {
+        return std::filesystem::path(path).extension() == extension || std::filesystem::path(path).filename() == extension;
+    }
+
+    std::string CombinePaths(const std::string& path1, const std::string& path2)
+    {
+        return (std::filesystem::path(path1) / std::filesystem::path(path2)).string();
     }
 }
