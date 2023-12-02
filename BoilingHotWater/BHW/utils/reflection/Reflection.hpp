37bb09f55cea3dc1325128282478a09aa95e94cb
NextLegacy <mcserezli@gmail.com>
2023-12-02 20:51:32 +0100

    Add missing includes for reflection types

--- BoilingHotWater/BHW/utils/reflection/Reflection.hpp
+++ BoilingHotWater/BHW/utils/reflection/Reflection.hpp
@@ -2,57 +2,11 @@
 
 #include "BHW/utils/reflection/Member.hpp"
 #include "BHW/utils/reflection/Value.hpp"
+#include "BHW/utils/reflection/Type.hpp"
+#include "BHW/utils/reflection/TypeInfo.hpp"
 #include "BHW/utils/reflection/ReflectionDefinitions.hpp"
 
-/*namespace BHW 
-{ 
-    namespace __reflection 
-    { 
-        inline static const TypeInfo& __33 = MakeTypeInfo<bool>(); 
-    } 
-    inline constexpr auto Cast(void* ptr) { return static_cast<bool*>(ptr); } 
-    inline constexpr const TypeInfo& GetTypeInfo() { return __reflection::__33; } 
-    inline constexpr bool IsRegistered() { return true; } 
-}*/
-/*
-namespace BHW 
-{ 
-    namespace __reflection 
-    { 
-        inline static const TypeInfo& __33 = MakeTypeInfo<bool>(); 
-    } 
-    //template <> 
-    //inline constexpr auto Cast<TypeHash<bool>()>(void* ptr) 
-    //{ 
-    //    return static_cast<bool*>(ptr); 
-    //} 
-    template <> 
-    inline constexpr const TypeInfo& GetTypeInfo<TypeHash<bool>()>() 
-    { 
-        return __reflection::__33; 
-    } 
-    template <> inline constexpr bool IsRegistered<bool>() { return true; } 
-}
-
-*/
-
-namespace BHW 
-{ 
-    template <uint64_t T>
-    constexpr auto Test(void* ptr)
-    {
-        BHW_DEBUG("Type with hash {} is not registered", T);
-    }
-
-    template <>
-    inline constexpr auto Test<TypeHash<uint64_t>()>(void* ptr)
-    {
-        // Specialization for bool
-        return static_cast<bool*>(ptr);
-    }
-}
-//BHW_REFLECT(bool              );
-/*
+BHW_REFLECT(bool              );
 BHW_REFLECT(char              );
 BHW_REFLECT(unsigned char     );
 BHW_REFLECT(short             );
@@ -68,7 +22,7 @@ BHW_REFLECT(double            );
 BHW_REFLECT(long double       );
 BHW_REFLECT(std::string       );
 BHW_REFLECT(std::string_view  );
-*/
+
 /*
 BHW_REFLECT(std::vector<bool              >);
 BHW_REFLECT(std::vector<char              >);
