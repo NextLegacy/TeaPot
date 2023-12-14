e910274f699462f2840bab8ca9b1fb0bc11a6235
NextLegacy <mcserezli@gmail.com>
2023-12-14 20:56:53 +0100

    removed comments

--- TeaPot/TP/application/ecs/systems/DebugSystem.hpp
+++ TeaPot/TP/application/ecs/systems/DebugSystem.hpp
@@ -2,31 +2,6 @@
 
 #include "TP/application/EventSubscriber.hpp"
 
-#include <BHW/utils/reflection/Reflection.hpp>
-
-struct SomeTest
-{
-    uint64_t abc;
-    std::string hello_world;
-    std::vector<std::string> test;
-    std::vector<std::vector<std::string>> test2;
-
-    SomeTest(uint64_t abc, std::string hello_world, std::vector<std::string> test, std::vector<std::vector<std::string>> test2) 
-        : abc(abc), hello_world(hello_world), test(test), test2(test2) { }
-
-    SomeTest() : SomeTest(0, "", { }, { }) { }
-};
-
-/*
-BHW_REFLECT(SomeTest)
-
-BHW_REFLECT_MEMBERS(SomeTest,
-    BHW_REFLECT_MEMBER("abc"        , &SomeTest::abc        ),
-    BHW_REFLECT_MEMBER("hello_world", &SomeTest::hello_world),
-    BHW_REFLECT_MEMBER("test"       , &SomeTest::test       ),
-    BHW_REFLECT_MEMBER("test2"      , &SomeTest::test2      )
-)*/
-
 namespace TP
 {
     class DebugSystem : public EventSubscriber
