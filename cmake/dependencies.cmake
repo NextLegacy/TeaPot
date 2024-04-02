0748667f25d8d51da7e97f1b68c19599f7d608a8
NextLegacy <mcserezli@gmail.com>
2024-04-02 19:44:59 +0200

    Remove submodules and dependencies

--- cmake/dependencies.cmake
+++
@@ -1,9 +0,0 @@
-add_subdirectory(dependencies/BoilingHotWater)
-add_subdirectory(dependencies/TeaCup)
-
-
-#list(APPEND CMAKE_PREFIX_PATH dependencies/BoilingHotWater/install)
-#find_package(BHW REQUIRED)
-
-#list(APPEND CMAKE_PREFIX_PATH dependencies/TeaCup/install)
-#find_package(TC REQUIRED)
